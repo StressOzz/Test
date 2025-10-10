6664fcc0b6dfd287ba8bd89f25f115f8a2881e76
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-10-10 20:52:37 +0500

    Update and rename test10.sh to test11.sh

--- test11.sh
+++ test11.sh
@@ -6,7 +6,7 @@ TMP_DIR="/tmp/podkop_install"
 
 rm -rf "$TMP_DIR"
 mkdir -p "$TMP_DIR"
-
+clear
 echo "Скачиваем список пакетов последнего релиза..."
 URLS=$(wget -qO- "https://api.github.com/repos/$REPO/releases/latest" | grep -o 'https://[^"]*\.ipk')
 
@@ -16,23 +16,23 @@ for url in $URLS; do
     wget -q -O "$file" "$url"
 done
 
-echo "Устанавливаем podkop..."
-for file in "$TMP_DIR"/podkop*.ipk; do
-    [ -f "$file" ] && opkg install "$file" >/dev/null 2>&1
-done
+install_pkg() {
+    pkg="$1"
+    echo "Устанавливаем $pkg..."
+    for file in "$TMP_DIR"/$pkg*.ipk; do
+        if [ -f "$file" ]; then
+            opkg install "$file" || echo "Не удалось установить $file (возможна несовместимая архитектура или зависимость)"
+        fi
+    done
+}
 
-echo "Устанавливаем luci-app..."
-for file in "$TMP_DIR"/luci-app*.ipk; do
-    [ -f "$file" ] && opkg install "$file" >/dev/null 2>&1
-done
+install_pkg "podkop"
+install_pkg "luci-app"
 
 read -p "Устанавливать русский язык для Podkop? (y/n) " RUS
 case "$RUS" in
     y|Y)
-        echo "Устанавливаем русскую локализацию..."
-        for file in "$TMP_DIR"/luci-i18n*.ipk; do
-            [ -f "$file" ] && opkg install "$file" >/dev/null 2>&1
-        done
+        install_pkg "luci-i18n"
         ;;
     *) echo "Русский язык не будет установлен." ;;
 esac
