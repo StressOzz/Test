55a8b7d0622bb1d60e0139a2af832604aff41374
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-10-10 20:30:11 +0500

    Update and rename test21.sh to test.sh

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+REPO="itdoginfo/podkop"
+TMP_DIR="/tmp/podkop_install"
+
+rm -rf "$TMP_DIR"
+mkdir -p "$TMP_DIR"
+
+LATEST_TAG=$(wget -qO- "https://api.github.com/repos/$REPO/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+
+FILES=(
+    "luci-app-podkop-${LATEST_TAG}-all.ipk"
+    "luci-i18n-podkop-ru-${LATEST_TAG}.ipk"
+    "podkop-${LATEST_TAG}-all.ipk"
+)
+
+for FILE in "${FILES[@]}"; do
+    wget -q -P "$TMP_DIR" "https://github.com/$REPO/releases/download/$LATEST_TAG/$FILE"
+done
+
+opkg install "$TMP_DIR"/*.ipk
+
+rm -rf "$TMP_DIR"
