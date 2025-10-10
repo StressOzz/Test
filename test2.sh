2761e5f95c33a52a0607115c1fc60b1eedc7add9
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-10-10 20:32:08 +0500

    Update and rename test1.sh to test2.sh

--- test2.sh
+++ test2.sh
@@ -7,20 +7,16 @@ TMP_DIR="/tmp/podkop_install"
 rm -rf "$TMP_DIR"
 mkdir -p "$TMP_DIR"
 
-# Получаем последний тег без -P
+# Получаем последний тег
 LATEST_TAG=$(wget -qO- "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' | head -n1 | cut -d'"' -f4)
 
-FILES=(
-    "luci-app-podkop-${LATEST_TAG}-all.ipk"
-    "luci-i18n-podkop-ru-${LATEST_TAG}.ipk"
-    "podkop-${LATEST_TAG}-all.ipk"
-)
+# Список файлов через пробел
+FILES="luci-app-podkop-${LATEST_TAG}-all.ipk luci-i18n-podkop-ru-${LATEST_TAG}.ipk podkop-${LATEST_TAG}-all.ipk"
 
-for FILE in "${FILES[@]}"; do
+for FILE in $FILES; do
     wget -q -P "$TMP_DIR" "https://github.com/$REPO/releases/download/$LATEST_TAG/$FILE"
 done
 
 opkg install "$TMP_DIR"/*.ipk
 
 rm -rf "$TMP_DIR"
-
