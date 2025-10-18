a79d1783ce014dd24e49b2a97cdd9a4ac54b75a1
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-10-18 22:52:09 +0500

    Update and rename manager2.sh to manager3.sh

--- manager3.sh
+++ manager3.sh
@@ -275,13 +275,19 @@ enable_discord_calls() {
 		sleep 1
 				enable_discord_calls
 				;;
-            *)
-                echo -e ""
-                echo -e "${GREEN}Выходим в главное меню...${NC}"
-				sleep 1
-                return
-                ;;
-        esac
+  			  0|"")
+        			echo -e ""
+       				echo -e "${GREEN}Выходим в главное меню...${NC}"
+        			sleep 1
+        			show_menu
+        			;;
+    		*)
+        			echo -e ""
+        			echo -e "${RED}Неверный выбор. Возврат в главное меню.${NC}"
+        			sleep 1
+        			show_menu
+        			;;
+esac
     fi
 
     # Если выбранный уже установлен, не скачиваем
