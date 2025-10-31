527f15eecc3f5083c15ecfccda892b2ba700f4e2
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-10-31 19:37:12 +0500

    Rename manager16.sh to manager17.sh

--- manager17.sh
+++ manager17.sh
@@ -731,7 +731,7 @@ fi
 
 CONF="/etc/config/zapret"
 if grep -q "option NFQWS_PORTS_UDP.*20000-22000" "$CONF" && grep -q -- "--filter-udp=20000-22000" "$CONF"; then
-    echo -e "\n${YELLOW}Стратегия для Battlefield REDSEC: ${NC}активна"
+    echo -e "\n${YELLOW}Стратегия для Battlefield REDSEC активна${NC}"
 fi
 
     echo -e ""
