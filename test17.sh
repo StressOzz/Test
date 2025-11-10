37794f813eca0c9d63760a98710bd269b9567ae0
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-11-10 19:56:35 +0500

    Update and rename test16.sh to test17.sh

--- test17.sh
+++ test17.sh
@@ -152,13 +152,13 @@ SCRIPT_FILE="/opt/zapret/init.d/openwrt/custom.d/50-script.sh"
 [ -f "$SCRIPT_FILE" ] || return
 line=$(head -n1 "$SCRIPT_FILE")
 if echo "$line" | grep -q "QUIC"; then
-echo -e "\n${YELLOW}Установлен скрипт: ${NC}50-quic4all"
+echo -e "${YELLOW}Установлен скрипт: ${NC}50-quic4all\n"
 elif echo "$line" | grep -q "stun"; then
-echo -e "\n${YELLOW}Установлен скрипт: ${NC}50-stun4all"
+echo -e "${YELLOW}Установлен скрипт: ${NC}50-stun4all\n"
 elif echo "$line" | grep -q "discord media"; then
-echo -e "\n${YELLOW}Установлен скрипт: ${NC}50-discord-media"
+echo -e "${YELLOW}Установлен скрипт: ${NC}50-discord-media\n"
 elif echo "$line" | grep -q "discord subnets"; then
-echo -e "\n${YELLOW}Установлен скрипт: ${NC}50-discord"
+echo -e "${YELLOW}Установлен скрипт: ${NC}50-discord\n"
     fi
 }
 # ==========================================
