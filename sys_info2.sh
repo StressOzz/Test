e393d9add53be14e5f6b5cc5cc69eb3d449ed35b
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-11-23 19:17:26 +0500

    Update and rename sys_info1.sh to sys_info2.sh

--- sys_info2.sh
+++ sys_info2.sh
@@ -15,11 +15,11 @@ CONF="/etc/config/zapret"
 CUSTOM_DIR="/opt/zapret/init.d/openwrt/custom.d/"
 
 clear
-echo -e "${GREEN}===== Model =====${NC}"; cat /tmp/sysinfo/model;
-echo -e "\n${GREEN}===== OpenWrt =====${NC}"; awk -F= '/DISTRIB_DESCRIPTION/ {gsub(/'\''/, ""); print "Версия OpenWrt: "$2} /DISTRIB_ARCH/ {gsub(/'\''/, ""); print "Процессор: "$2} /DISTRIB_TARGET/ {gsub(/'\''/, ""); print "Платформа: "$2}' /etc/openwrt_release;
+echo -e "${GREEN}===== Model =====${NC}"; cat /tmp/sysinfo/model; awk -F= '/DISTRIB_ARCH/ {gsub(/'\''/, ""); print ""$2} /DISTRIB_TARGET/ {gsub(/'\''/, ""); print ""$2}' /etc/openwrt_release;
+echo -e "\n${GREEN}===== OpenWrt =====${NC}"; awk -F= '/DISTRIB_DESCRIPTION/ {gsub(/'\''|OpenWrt /,""); print $2}' /etc/openwrt_release;
 echo -e "\n${GREEN}===== User Packages =====${NC}"; awk '/^Package:/ {p=$2} /^Status: install user/ {print p}' /usr/lib/opkg/status;
 echo -e "\n${GREEN}===== Flow Offloading =====${NC}"; sw=$(uci -q get firewall.@defaults[0].flow_offloading); hw=$(uci -q get firewall.@defaults[0].flow_offloading_hw); echo -e "SW: ${sw:+on}${sw:-off} | HW: ${hw:+on}${hw:-off}";
-echo -e "\n${GREEN}===== GitHub API Rate Limit =====${NC}"; echo -e "Core remaining: $(curl -s https://api.github.com/rate_limit | sed -n 's/.*\"remaining\": \([0-9]*\).*/\1/p' | head -1)\n"
+echo -e "\n${GREEN}===== GitHub API Rate Limit =====${NC}"; echo -e "Limit remaining: $(curl -s https://api.github.com/rate_limit | sed -n 's/.*\"remaining\": \([0-9]*\).*/\1/p' | head -1)\n"
 
 #!/bin/sh
 # simple_check_sites.sh — список встроен прямо в скрипт
@@ -52,7 +52,7 @@ echo "$SITES" | while IFS= read -r site; do
         ""|\#*) continue ;;
     esac
 
-    if curl -Is --connect-timeout 2 --max-time 3 "https://$site" >/dev/null 2>&1; then
+    if curl -Is --connect-timeout 1 --max-time 2 "https://$site" >/dev/null 2>&1; then
         echo "[OK]   $site"
     else
         echo "[FAIL] $site"
