12bee0c12976dae8803ae227a982332ec1708434
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-11-23 20:28:07 +0500

    Update and rename sys_info17.sh to sys_info18.sh

--- sys_info18.sh
+++ sys_info18.sh
@@ -100,8 +100,8 @@ for idx in $(seq 1 $half); do
     right=$(echo $sites_list | cut -d' ' -f$right_idx)
 
     # printf только задает отступы
-    left_pad=$(printf "%-35s" "$left")
-    right_pad=$(printf "%-35s" "$right")
+    left_pad=$(printf "%-25s" "$left")
+    right_pad=$(printf "%-25s" "$right")
 
     # затем echo с цветом
     if curl -Is --connect-timeout 1 --max-time 2 "https://$left" >/dev/null 2>&1; then
@@ -114,7 +114,7 @@ for idx in $(seq 1 $half); do
         if curl -Is --connect-timeout 1 --max-time 2 "https://$right" >/dev/null 2>&1; then
             right_color="${GREEN}OK${NC}"
         else
-            right_color="${RED}FAIL${NC}"
+            right_color="${RED}NO${NC}"
         fi
         echo -e "[$left_color] $left_pad [$right_color] $right_pad"
     else
