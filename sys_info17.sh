9b750072cd821558006a8dac1b12df2f6f82a575
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-11-23 20:26:34 +0500

    Update and rename sys_info16.sh to sys_info17.sh

--- sys_info17.sh
+++ sys_info17.sh
@@ -99,26 +99,28 @@ for idx in $(seq 1 $half); do
     right_idx=$((idx + half))
     right=$(echo $sites_list | cut -d' ' -f$right_idx)
 
-    # левый
+    # printf только задает отступы
+    left_pad=$(printf "%-35s" "$left")
+    right_pad=$(printf "%-35s" "$right")
+
+    # затем echo с цветом
     if curl -Is --connect-timeout 1 --max-time 2 "https://$left" >/dev/null 2>&1; then
-        left_code="${GREEN}OK${NC}"
+        left_color="${GREEN}OK${NC}"
     else
-        left_code="${RED}FAIL${NC}"
+        left_color="${RED}FAIL${NC}"
     fi
 
-    # правый
     if [ -n "$right" ]; then
         if curl -Is --connect-timeout 1 --max-time 2 "https://$right" >/dev/null 2>&1; then
-            right_code="${GREEN}OK${NC}"
+            right_color="${GREEN}OK${NC}"
         else
-            right_code="${RED}FAIL${NC}"
+            right_color="${RED}FAIL${NC}"
         fi
-        echo -e "[${left_code}] $left  [${right_code}] $right"
+        echo -e "[$left_color] $left_pad [$right_color] $right_pad"
     else
-        echo -e "[${left_code}] $left"
+        echo -e "[$left_color] $left_pad"
     fi
 done
-
 
 echo ""
 read -p "Нажмите Enter для выхода в главное меню..." dummy
