f173aec04d1e7a5b678722c99229acc5855447d8
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-06 22:35:11 +0500

    Update and rename test22.sh to test23.sh

--- test23.sh
+++ test23.sh
@@ -22,32 +22,9 @@ fi
     ttyd_login_have="login"
  fi
  
- if [[ "$OSystem" == "VPS" ]]; then
-	echo -e "${yellow}Установка ttyd for VPS${plain}"
-	systemctl stop ttyd 2>/dev/null || true
-	curl -L -o /usr/bin/ttyd https://github.com/tsl0922/ttyd/releases/latest/download/ttyd.x86_64
-	chmod +x /usr/bin/ttyd
-	
-	cat > /etc/systemd/system/ttyd.service <<EOF
-[Unit]
-Description=ttyd WebSSH Service
-After=network.target
 
-[Service]
-ExecStart=/usr/bin/ttyd -p 17681 -W -a ${ttyd_login_have}
-Restart=always
-RestartSec=5
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-	systemctl daemon-reload
-	systemctl enable ttyd
-	systemctl start ttyd
- elif [[ "$OSystem" == "WRT" ]]; then
 	echo -e "${yellow}Установка ttyd for WRT${plain}"
-	/etc/init.d/ttyd stop 2>/dev/null || true
+	/etc/init.d/ttyd stop
 	opkg update
 	opkg install ttyd
     uci set ttyd.@ttyd[0].interface=''
@@ -57,46 +34,12 @@ EOF
 	
 	/etc/init.d/ttyd enable
 	/etc/init.d/ttyd start
- elif [[ "$OSystem" == "entware" ]]; then
-	echo -e "${yellow}Установка ttyd for Entware${plain}"
-	/opt/etc/init.d/S99ttyd stop 2>/dev/null || true
-	opkg install ttyd
-	
-	cat > /opt/etc/init.d/S99ttyd <<EOF
-#!/bin/sh
 
-START=99
-
-case "\$1" in
-  start)
-    echo "Starting ttyd..."
-    ttyd -p 17681 -W -a ${ttyd_login_have} &
-    ;;
-  stop)
-    echo "Stopping ttyd..."
-    killall ttyd
-    ;;
-  restart)
-    \$0 stop
-    sleep 1
-    \$0 start
-    ;;
-  *)
-    echo "Usage: \$0 {start|stop|restart}"
-    exit 1
-    ;;
-esac
-EOF
-
-  chmod +x /opt/etc/init.d/S99ttyd
-  /opt/etc/init.d/S99ttyd start
-  sleep 1
   if netstat -tuln | grep -q ':17681'; then
 	echo -e "${green}Порт 17681 для службы ttyd слушается${plain}"
   else
 	echo -e "${red}Порт 17681 для службы ttyd не прослушивается${plain}"
   fi
- fi
 
  if pidof ttyd >/dev/null; then
 	echo -e "Проверка...${green}Служба ttyd запущена.${plain}"
