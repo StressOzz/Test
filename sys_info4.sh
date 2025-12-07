83ceaeca5d231ab1edccfd3823649b167594d637
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-07 17:51:19 +0500

    Update and rename sys_info3.sh to sys_info4.sh

--- sys_info4.sh
+++ sys_info4.sh
@@ -27,10 +27,7 @@ echo -e "TCP: ${GREEN}$TCP_VAL${NC} | UDP: ${GREEN}$UDP_VAL${NC}"; echo -e "\n${
 awk ' /^[[:space:]]*option[[:space:]]+NFQWS_OPT[[:space:]]*'\''/ {flag=1; sub(/^[[:space:]]*option[[:space:]]+NFQWS_OPT[[:space:]]*'\''/, ""); next}
 flag { if (/'\''/) {sub(/'\''$/, ""); print; exit} print }' "$CONF"; }
 if [ -f /etc/init.d/zapret ]; then zpr_info; else echo -e "${RED}Zapret не установлен!${NC}\n"; fi; echo -e "${GREEN}===== Доступность сайтов =====${NC}"
-SITES=$(cat <<'EOF'
-gosuslugi.ru esia.gosuslugi.ru rutube.ru youtube.com instagram.com rutor.info ntc.party rutracker.org epidemz.net.co nnmclub.to openwrt.org sxyprn.net pornhub.com discord.com x.com filmix.my flightradar24.com cdn77.com play.google.com genderize.io
-EOF
-)
+SITES="gosuslugi.ru esia.gosuslugi.ru rutube.ru youtube.com instagram.com rutor.info ntc.party rutracker.org epidemz.net.co nnmclub.to openwrt.org sxyprn.net pornhub.com discord.com x.com filmix.my flightradar24.com cdn77.com play.google.com genderize.io"
 sites_clean=$(echo "$SITES" | grep -v '^#' | grep -v '^\s*$'); total=$(echo "$sites_clean" | wc -l); half=$(( (total + 1) / 2 )); sites_list=""
 for site in $sites_clean; do sites_list="$sites_list $site"; done
 for idx in $(seq 1 $half); do left=$(echo $sites_list | cut -d' ' -f$idx); right_idx=$((idx + half)); right=$(echo $sites_list | cut -d' ' -f$right_idx); left_pad=$(printf "%-25s" "$left")
