fa8b8d8360a2e37b58ca2943d8c2a16fbccd989b
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-07 17:54:44 +0500

    Update and rename sys_info5.sh to sys_info6.sh

--- sys_info6.sh
+++ sys_info6.sh
@@ -50,7 +50,7 @@ play.google.com
 genderize.io
 EOF
 )
-sites_clean=$(echo "$SITES" | grep -v '^#' | grep -v '^\s*$'); total=$(echo "$sites_clean" | wc -l); half=$(( (total + 1) / 2 )); sites_list=""
+sites_clean=$(echo "$SITES" | grep -v '^#' | grep -v '^\s*$'); total=$(echo "$sites_clean" | wc -w); half=$(( (total + 1) / 2 )); sites_list=""
 for site in $sites_clean; do sites_list="$sites_list $site"; done
 for idx in $(seq 1 $half); do left=$(echo $sites_list | cut -d' ' -f$idx); right_idx=$((idx + half)); right=$(echo $sites_list | cut -d' ' -f$right_idx); left_pad=$(printf "%-25s" "$left")
 right_pad=$(printf "%-25s" "$right"); if curl -Is --connect-timeout 3 --max-time 4 "https://$left" >/dev/null 2>&1; then left_color="[${GREEN}OK${NC}]  "
