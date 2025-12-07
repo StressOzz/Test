55cf7e0ffcb16ceaca755c2bf096d9c24de7fe65
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-07 17:46:56 +0500

    Update and rename sys_info1.sh to sys_info2.sh

--- sys_info2.sh
+++ sys_info2.sh
@@ -15,35 +15,19 @@ out="$out | FIX: ${dpi}"; echo -e "$out\n\n${GREEN}===== Проверка GitHub
 [ -z "$RATE" ] && RATE_OUT="${RED}N/A${NC}" || RATE_OUT=$([ "$RATE" -eq 0 ] && echo -e "${RED}0${NC}" || echo -e "${GREEN}$RATE${NC}"); echo -n "IPv4: "
 curl -4 -Is --connect-timeout 3 https://github.com >/dev/null 2>&1 && echo -ne "${GREEN}ok${NC}" || echo -ne "${RED}fail${NC}"; echo -n "  IPv6: "
 curl -6 -Is --connect-timeout 3 https://github.com >/dev/null 2>&1 && echo -e "${GREEN}ok${NC}" || echo -e "${RED}fail${NC}"; echo -n "API: "
-curl -Is --connect-timeout 3 https://api.github.com >/dev/null 2>&1 \
-&& echo -e "${GREEN}ok${NC}   Limit: $RATE_OUT" \
-|| echo -e "${RED}fail${NC}   Limit: $RATE_OUT"
+curl -Is --connect-timeout 3 https://api.github.com >/dev/null 2>&1 \ && echo -e "${GREEN}ok${NC}   Limit: $RATE_OUT" \ || echo -e "${RED}fail${NC}   Limit: $RATE_OUT"
 echo -e "\n${GREEN}===== Настройки Zapret =====${NC}"
-zpr_info() { INSTALLED_VER=$(opkg list-installed | grep '^zapret ' | awk '{print $3}')
-if /etc/init.d/zapret status 2>/dev/null | grep -qi "running"; then ZAPRET_STATUS="${GREEN}запущен${NC}"
-else
-ZAPRET_STATUS="${RED}остановлен${NC}"; fi
+zpr_info() { INSTALLED_VER=$(opkg list-installed | grep '^zapret ' | awk '{print $3}'); if /etc/init.d/zapret status 2>/dev/null | grep -qi "running"; then ZAPRET_STATUS="${GREEN}запущен${NC}"
+else ZAPRET_STATUS="${RED}остановлен${NC}"; fi
 SCRIPT_FILE="/opt/zapret/init.d/openwrt/custom.d/50-script.sh"; if [ -f "$SCRIPT_FILE" ]; then line=$(head -n1 "$SCRIPT_FILE")
-case "$line" in
-*QUIC*) name="50-quic4all" ;; *stun*) name="50-stun4all" ;; *"discord media"*) name="50-discord-media" ;; *"discord subnets"*) name="50-discord" ;; *) name="" ;;
-esac
-fi
-TCP_VAL=$(grep -E "^[[:space:]]*option NFQWS_PORTS_TCP[[:space:]]+'" "$CONF" \
-| sed "s/.*'\(.*\)'.*/\1/")
-UDP_VAL=$(grep -E "^[[:space:]]*option NFQWS_PORTS_UDP[[:space:]]+'" "$CONF" \
-| sed "s/.*'\(.*\)'.*/\1/")
+case "$line" in *QUIC*) name="50-quic4all" ;; *stun*) name="50-stun4all" ;; *"discord media"*) name="50-discord-media" ;; *"discord subnets"*) name="50-discord" ;; *) name="" ;; esac fi
+TCP_VAL=$(grep -E "^[[:space:]]*option NFQWS_PORTS_TCP[[:space:]]+'" "$CONF" \ | sed "s/.*'\(.*\)'.*/\1/")
+UDP_VAL=$(grep -E "^[[:space:]]*option NFQWS_PORTS_UDP[[:space:]]+'" "$CONF" \ | sed "s/.*'\(.*\)'.*/\1/")
 echo -e "${GREEN}$INSTALLED_VER${NC} | $ZAPRET_STATUS"; [ -n "$name" ] && echo -e "${GREEN}$name${NC}"
 echo -e "TCP: ${GREEN}$TCP_VAL${NC} | UDP: ${GREEN}$UDP_VAL${NC}"; echo -e "\n${GREEN}===== Стратегия =====${NC}"
-awk '
-/^[[:space:]]*option[[:space:]]+NFQWS_OPT[[:space:]]*'\''/ {flag=1; sub(/^[[:space:]]*option[[:space:]]+NFQWS_OPT[[:space:]]*'\''/, ""); next}
-flag {
-if (/'\''/) {sub(/'\''$/, ""); print; exit}
-print
-}' "$CONF"; }
-if [ -f /etc/init.d/zapret ]; then zpr_info
-else
-echo -e "${RED}Zapret не установлен!${NC}\n"; fi
-echo -e "${GREEN}===== Доступность сайтов =====${NC}"
+awk ' /^[[:space:]]*option[[:space:]]+NFQWS_OPT[[:space:]]*'\''/ {flag=1; sub(/^[[:space:]]*option[[:space:]]+NFQWS_OPT[[:space:]]*'\''/, ""); next}
+flag { if (/'\''/) {sub(/'\''$/, ""); print; exit} print }' "$CONF"; }
+if [ -f /etc/init.d/zapret ]; then zpr_info; else echo -e "${RED}Zapret не установлен!${NC}\n"; fi; echo -e "${GREEN}===== Доступность сайтов =====${NC}"
 SITES=$(cat <<'EOF'
 gosuslugi.ru
 esia.gosuslugi.ru
@@ -68,17 +52,8 @@ genderize.io
 EOF
 )
 sites_clean=$(echo "$SITES" | grep -v '^#' | grep -v '^\s*$'); total=$(echo "$sites_clean" | wc -l); half=$(( (total + 1) / 2 )); sites_list=""
-for site in $sites_clean; do
-sites_list="$sites_list $site"
-done
-for idx in $(seq 1 $half); do
-left=$(echo $sites_list | cut -d' ' -f$idx); right_idx=$((idx + half)); right=$(echo $sites_list | cut -d' ' -f$right_idx); left_pad=$(printf "%-25s" "$left")
-right_pad=$(printf "%-25s" "$right")
-if curl -Is --connect-timeout 3 --max-time 4 "https://$left" >/dev/null 2>&1; then left_color="[${GREEN}OK${NC}]  "
-else
-left_color="[${RED}FAIL${NC}]"; fi; if [ -n "$right" ]; then if curl -Is --connect-timeout 3 --max-time 4 "https://$right" >/dev/null 2>&1; then right_color="[${GREEN}OK${NC}]  "
-else
-right_color="[${RED}FAIL${NC}]"; fi
-echo -e "$left_color $left_pad $right_color $right_pad"
-else
-echo -e "$left_color $left_pad"; fi; done
+for site in $sites_clean; do sites_list="$sites_list $site"; done
+for idx in $(seq 1 $half); do left=$(echo $sites_list | cut -d' ' -f$idx); right_idx=$((idx + half)); right=$(echo $sites_list | cut -d' ' -f$right_idx); left_pad=$(printf "%-25s" "$left")
+right_pad=$(printf "%-25s" "$right"); if curl -Is --connect-timeout 3 --max-time 4 "https://$left" >/dev/null 2>&1; then left_color="[${GREEN}OK${NC}]  "
+else left_color="[${RED}FAIL${NC}]"; fi; if [ -n "$right" ]; then if curl -Is --connect-timeout 3 --max-time 4 "https://$right" >/dev/null 2>&1; then right_color="[${GREEN}OK${NC}]  "
+else right_color="[${RED}FAIL${NC}]"; fi; echo -e "$left_color $left_pad $right_color $right_pad"; else echo -e "$left_color $left_pad"; fi; done
