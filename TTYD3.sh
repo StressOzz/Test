59bee969de08486fa0033de88e874c031b291f3f
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-09 23:27:00 +0500

    Update and rename TTYD2.sh to TTYD3.sh

--- TTYD3.sh
+++ TTYD3.sh
@@ -3,4 +3,4 @@ echo -e 'sh <(wget -O - https://raw.githubusercontent.com/StressOzz/Zapret-Manag
 chmod +x /usr/bin/zms; echo -e "\n\033[1;32mУстановка \033[0mttyd"; opkg update >/dev/null 2>&1;opkg install ttyd >/dev/null 2>&1
 uci set ttyd.@ttyd[0].interface=''; uci set ttyd.@ttyd[0].command="-W -a sh /usr/bin/zms"; uci commit ttyd >/dev/null 2>&1
 /etc/init.d/ttyd enable >/dev/null 2>&1; /etc/init.d/ttyd start >/dev/null 2>&1
-if pidof ttyd >/dev/null; then echo -e "\033[1;32mСлужба ttyd запущена.\nДоступ: \033[0mhttp://192.168.1.1:7681\n"; else echo -e "\033[1;31mСлужба ttyd не запущена!\033[0m"; fi
+if pidof ttyd >/dev/null; then echo -e "\033[1;32mСлужба запущена!\nДоступ: \033[0mhttp://192.168.1.1:7681\n"; else echo -e "\033[1;31mСлужба не запущена!\033[0m"; fi
