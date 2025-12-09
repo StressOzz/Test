d9b7ad019d81f62c50ca8ad18b08b8996bb9d013
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-09 20:39:31 +0500

    Update and rename test10.sh to test11.sh

--- test11.sh
+++ test11.sh
@@ -5,12 +5,12 @@ chmod +x /usr/bin/zms
 
 echo -e "\033[1;36mУстановка ttyd for WRT\033[0m"
 
-/etc/init.d/ttyd stop >/dev/null 2>&1 || true
+/etc/init.d/ttyd stop
 opkg update
-opkg install ttyd bash
+opkg install ttyd
 
 uci set ttyd.@ttyd[0].interface=''
-uci set ttyd.@ttyd[0].command="-p 17681 -W -a -c : bash zms"
+uci set ttyd.@ttyd[0].command="-W -a sh /usr/bin/zms"
 uci commit ttyd
 
 /etc/init.d/ttyd enable
@@ -19,7 +19,7 @@ uci commit ttyd
 if pidof ttyd >/dev/null; then
     echo -e "\033[1;32mСлужба ttyd запущена.\033[0m"
 else
-    echo -e "\033[1;31mСлужба ttyd не запущена! На Entware она часто стартует после перезагрузки.\033[0m"
+    echo -e "\033[1;31mСлужба ttyd не запущена!\033[0m"
 fi
 
-echo -e "\033[1;33mУстановка завершена. Доступ: http://192.168.1.1:17681\033[0m"
+echo -e "\033[1;33mУстановка завершена. Доступ: http://192.168.1.1:7681\033[0m"
