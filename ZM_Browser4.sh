735d9a046b8edfd45888df26d00c14d8405f507a
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-11 01:08:48 +0500

    Update and rename ZM_Browser3.sh to ZM_Browser4.sh

--- ZM_Browser4.sh
+++ ZM_Browser4.sh
@@ -5,8 +5,8 @@ RED="\033[1;31m"
 CYAN="\033[1;36m"
 YELLOW="\033[1;33m"
 NC="\033[0m"
-echo
-echo; clear
+
+clear
 
 echo -e 'sh <(wget -O - https://raw.githubusercontent.com/StressOzz/Zapret-Manager/main/Zapret-Manager.sh)' > /usr/bin/zms
 chmod +x /usr/bin/zms
