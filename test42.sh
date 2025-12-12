8727b10873ac6765b39cc7ffaed194d7059b9855
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-12 22:42:52 +0500

    Update and rename test41.sh to test42.sh

--- test42.sh
+++ test42.sh
@@ -266,18 +266,11 @@ EOF
 )
     else
         echo -e "\n${MAGENTA}Возвращаем настройки по умолчанию${NC}"
-        extra_block=$(cat << 'EOF'
-config https-dns-proxy
-    option bootstrap_dns '1.1.1.1,1.0.0.1'
-    option resolver_url 'https://cloudflare-dns.com/dns-query'
-    option listen_port '5053'
+        
+extra_block=$(printf "%s\n" "config https-dns-proxy" "	option bootstrap_dns '1.1.1.1,1.0.0.1'" "	option resolver_url 'https://cloudflare-dns.com/dns-query'" \
+"	option listen_port '5053'" "" "config https-dns-proxy" "	option bootstrap_dns '8.8.8.8,8.8.4.4'" "	option resolver_url 'https://dns.google/dns-query'" \
+"	option listen_port '5054'" )
 
-config https-dns-proxy
-    option bootstrap_dns '8.8.8.8,8.8.4.4'
-    option resolver_url 'https://dns.google/dns-query'
-    option listen_port '5054'
-EOF
-)
     fi
     printf '%s\n' "config main 'config'" "	option canary_domains_icloud '1'" "	option canary_domains_mozilla '1'" "	option dnsmasq_config_update '*'" \
 "	option force_dns '1'" "	list force_dns_port '53'" "	list force_dns_port '853'" "	list force_dns_src_interface 'lan'" "	option procd_trigger_wan6 '0'" \
