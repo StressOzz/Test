4b0258e15e02af5eb94099a45d54656724b5336c
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-12 23:25:38 +0500

    Update and rename test.sh to test1.sh

---
+++ test1.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+wget -qO- https://github.com/remittor/zapret-openwrt/releases \
+  | grep '/remittor/zapret-openwrt/releases/tag/' \
+  | sed 's|.*/tag/||' \
+  | sort -V \
+  | tail -n1
