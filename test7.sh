789307e8de179e8211269ef8f95fddfa33aad68f
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-12 23:37:48 +0500

    Update and rename test7.sh to test8.sh

--- test7.sh
+++
@@ -1,5 +0,0 @@
-ZAPRET_VERSION=$(wget -qO- https://github.com/remittor/zapret-openwrt/releases \
-  | sed -n 's|.*href="/remittor/zapret-openwrt/releases/tag/v\([0-9.]*\)".*|\1|p' \
-  | head -n1)
-
-echo "$ZAPRET_VERSION"
