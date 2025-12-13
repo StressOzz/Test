97a9bc492f8cf0f863be9eee4c945729b1952a7b
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-13 22:17:02 +0500

    Update and rename test65.sh to test66.sh

--- test66.sh
+++ test66.sh
@@ -431,7 +431,7 @@ clear; echo -e "${MAGENTA}Системное меню${NC}\n"
 if quic_is_blocked; then
     echo -e "${YELLOW}Блокировка QUIC:${NC} ${GREEN}Включена${NC}"
 else
-    echo -e "${YELLOW}Блокировка QUIC:${NC} ${RED}Отключена${NC}"
+    echo -e "${YELLOW}Блокировка QUIC:${NC} ${GREEN}Отключена${NC}"
 fi
 
 echo -e "\n${CYAN}1) ${GREEN}Системная информация${NC}"
