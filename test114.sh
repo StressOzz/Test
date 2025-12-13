1aea9218d002fa9b92d1dfbf5a3c67d6abd4cb69
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-14 03:54:20 +0500

    Update and rename test113.sh to test114.sh

--- test114.sh
+++ test114.sh
@@ -164,6 +164,7 @@ printf '%s\n' "config main 'config'" "	option canary_domains_icloud '1'" "	optio
 "	option listen_addr '127.0.0.1'" "" "config https-dns-proxy" "	option resolver_url 'https://dns.comss.one/dns-query'" > "$fileDoH"
 /etc/init.d/https-dns-proxy enable >/dev/null 2>&1; /etc/init.d/https-dns-proxy restart >/dev/null 2>&1; echo -e "DNS over HTTPS${GREEN} установлен и настроен!${NC}\n"; fi; read -p "Нажмите Enter для выхода в главное меню..." dummy; }
 DoH_def(){
+[ ! -f /etc/init.d/https-dns-proxy ] && { echo -e "\n${RED}DNS over HTTPS не установлен!${NC}\n"; read -p "Нажмите Enter..." dummy; return; }
 echo -e "\n${MAGENTA}Возвращаем настройки по умолчанию${NC}"   
 printf '%s\n' "config main 'config'" "	option canary_domains_icloud '1'" "	option canary_domains_mozilla '1'" "	option dnsmasq_config_update '*'" \
 "	option force_dns '1'" "	list force_dns_port '53'" "	list force_dns_port '853'" "	list force_dns_src_interface 'lan'" "	option procd_trigger_wan6 '0'" \
