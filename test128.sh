3bef01f806a9e08ecfff9dcae135c11b878c70eb
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-14 05:12:35 +0500

    Update test128.sh

--- test128.sh
+++ test128.sh
@@ -163,23 +163,12 @@ printf '%s\n' "config main 'config'" "	option canary_domains_icloud '1'" "	optio
 "	list force_dns_src_interface 'lan'" "	option procd_trigger_wan6 '0'" "	option heartbeat_domain 'heartbeat.melmac.ca'" "	option heartbeat_sleep_timeout '10'" "	option heartbeat_wait_timeout '10'" "	option user 'nobody'" "	option group 'nogroup'" \
 "	option listen_addr '127.0.0.1'" "" "config https-dns-proxy" "	option resolver_url 'https://dns.comss.one/dns-query'" > "$fileDoH"
 /etc/init.d/https-dns-proxy enable >/dev/null 2>&1; /etc/init.d/https-dns-proxy restart >/dev/null 2>&1; echo -e "DNS over HTTPS${GREEN} установлен и настроен!${NC}\n"; fi; read -p "Нажмите Enter для выхода в главное меню..." dummy; }
-
-doh_st() {
-    if grep -q 'dns.comss.one' /etc/config/https-dns-proxy 2>/dev/null; then
-        comss_active=1
-        comss_text="${GREEN}Вернуть в ${NC}DNS over HTTPS ${GREEN}настройки по умолчанию${NC}"
-    else
-        comss_active=0
-        comss_text="${GREEN}Настроить ${NC}Comss DNS ${GREEN}в ${NC}DNS over HTTPS"
-    fi
-}
-
-
+doh_st() { if grep -q 'dns.comss.one' /etc/config/https-dns-proxy 2>/dev/null; then comss_active=1; comss_text="${GREEN}Вернуть ${NC}DNS over HTTPS ${GREEN}настройки по умолчанию${NC}"; else comss_active=0; comss_text="${GREEN}Настроить ${NC}DNS over HTTPS"; fi; }
 DoH_def(){ doh_st
 if ! opkg list-installed | grep -q '^https-dns-proxy '; then echo -e "\n${RED}DNS over HTTPS не установлен!${NC}\n"; read -p "Нажмите Enter..."; continue; fi
-rm -f "$fileDoH"; if [ "$comss_active" = 0 ]; then echo -e "\n${MAGENTA}Настраиваем Comss DNS${NC}"
+rm -f "$fileDoH"; if [ "$comss_active" = 0 ]; then echo -e "\n${MAGENTA}Настраиваем Comss DNS${NC}\n${CYAN}Применяем новые настройки"
 extra_block=$(printf "%s\n" "config https-dns-proxy" "	option resolver_url 'https://dns.comss.one/dns-query'")
-else echo -e "\n${MAGENTA}Возвращаем настройки по умолчанию${NC}"   
+else echo -e "\n${MAGENTA}Возвращаем настройки по умолчанию${NC}\n${CYAN}Возвращаем настройки к значениям по умолчанию${NC}"
 extra_block=$(printf "%s\n" "config https-dns-proxy" "	option bootstrap_dns '1.1.1.1,1.0.0.1'" "	option resolver_url 'https://cloudflare-dns.com/dns-query'" \
 "	option listen_port '5053'" "" "config https-dns-proxy" "	option bootstrap_dns '8.8.8.8,8.8.4.4'" "	option resolver_url 'https://dns.google/dns-query'" "	option listen_port '5054'")
 fi; printf '%s\n' "config main 'config'" "	option canary_domains_icloud '1'" "	option canary_domains_mozilla '1'" "	option dnsmasq_config_update '*'" \
@@ -188,7 +177,6 @@ fi; printf '%s\n' "config main 'config'" "	option canary_domains_icloud '1'" "	o
 "	option group 'nogroup'" "	option listen_addr '127.0.0.1'" "" "$extra_block" > "$fileDoH"
 /etc/init.d/https-dns-proxy stop >/dev/null 2>&1; /etc/init.d/https-dns-proxy start >/dev/null 2>&1; if [ "$comss_active" = 0 ]; then echo -e "Comss DNS ${GREEN}настроен!${NC}\n"
 else echo -e "${GREEN}Настройки по умолчанию возвращены!${NC}\n"; fi; read -p "Нажмите Enter..." dummy; }
-
 # ==========================================
 # Системное меню
 # ==========================================
