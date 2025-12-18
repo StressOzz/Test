ff472bcb244704cf43d5b9672d690940e9079605
StressOzz <131419324+StressOzz@users.noreply.github.com>
2025-12-19 01:42:11 +0500

    Update and rename test22.sh to test23.sh

--- test23.sh
+++ test23.sh
@@ -213,8 +213,23 @@ then if ! grep -q 'meta l4proto { tcp, udp } ct original packets ge 30 flow offl
 echo -e "${CYAN}4) ${GREEN}Применить ${NC}FIX${GREEN} для работы ${NC}Zapret${GREEN} с включённым ${NC}Flow Offloading${NC}"; fi; fi
 echo -ne "${CYAN}Enter) ${GREEN}Выход в главное меню${NC}\n\n${YELLOW}Выберите пункт:${NC} " && read -r choiceMN; case "$choiceMN" in
 1) wget -qO- https://raw.githubusercontent.com/StressOzz/Zapret-Manager/refs/heads/main/sys_info.sh | sh; echo; read -p "Нажмите Enter..." dummy ;;
-2) toggle_web ;; 3) toggle_quic ;; 4) echo -e "\n${MAGENTA}Применяем FIX для Flow Offloading${NC}"; sed -i 's/meta l4proto { tcp, udp } flow offload @ft;/meta l4proto { tcp, udp } ct original packets ge 30 flow offload @ft;/' /usr/share/firewall4/templates/ruleset.uc; fw4 restart >/dev/null 2>&1
-echo -e "FIX ${GREEN}успешно применён!${NC}\n"; read -p "Нажмите Enter..." dummy ;; *) echo; return ;; esac; done; }
+2) toggle_web ;; 3) toggle_quic ;; 
+4) if uci get firewall.@defaults[0].flow_offloading 2>/dev/null | grep -q '^1$' || \
+uci get firewall.@defaults[0].flow_offloading_hw 2>/dev/null | grep -q '^1$'; then
+if ! grep -q 'meta l4proto { tcp, udp } ct original packets ge 30 flow offload @ft;' /usr/share/firewall4/templates/ruleset.uc; then
+echo -e "\n${MAGENTA}Применяем FIX для Flow Offloading${NC}"
+sed -i 's/meta l4proto { tcp, udp } flow offload @ft;/meta l4proto { tcp, udp } ct original packets ge 30 flow offload @ft;/' /usr/share/firewall4/templates/ruleset.uc
+fw4 restart >/dev/null 2>&1
+echo -e "FIX ${GREEN}успешно применён!${NC}\n"
+read -p "Нажмите Enter..." dummy
+else
+echo -e "\n${YELLOW}FIX уже применён!${NC}\n"
+read -p "Нажмите Enter..." dummy
+fi
+else
+break
+fi
+;; *) echo; return ;; esac; done; }
 # ==========================================
 # Главное меню
 # ==========================================
