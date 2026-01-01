ba12361a3ddd8ada24d1f72e2f69f45cef3cfbfd
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-01-01 19:19:03 +0500

    Update and rename test150.sh to test151.sh

--- test151.sh
+++ test151.sh
@@ -42,36 +42,15 @@ read -p "Нажмите Enter..." dummy; return; } ; done; echo -e "${CYAN}Уд�
 # Меню настройки Discord
 # ==========================================
 pattern="104\.25\.158\.178 finland[0-9]\{5\}\.discord\.media"
-
-
-
 scrypt_install() { local NO_PAUSE=$1; [ ! -f /etc/init.d/zapret ] && { echo -e "\n${RED}Zapret не установлен!${NC}\n"; read -p "Нажмите Enter..." dummy; return; }
-while true; do [ "$NO_PAUSE" != "1" ] && clear && echo -e "${MAGENTA}Меню настройки Discord${NC}\n"
-
-output_shown=false
-
+while true; do [ "$NO_PAUSE" != "1" ] && clear && echo -e "${MAGENTA}Меню настройки Discord${NC}\n"; output_shown=false
 [ "$NO_PAUSE" != "1" ] && show_script_50 && [ -n "$name" ] && echo -e "${YELLOW}Установлен скрипт:${NC} $name" && output_shown=true
-
 grep -q "$pattern" /etc/hosts && echo -e "${YELLOW}Финские IP:${NC} включены" && output_shown=true
-
-toggle_finland_hosts() {
-    if grep -q "$pattern" /etc/hosts; then
-        sed -i "/$pattern/d" /etc/hosts
-echo -e "\n${MAGENTA}Удаляем Финские IP${NC}"
-        /etc/init.d/dnsmasq restart 2>/dev/null
-echo -e "${GREEN}Финские IP удалены${NC}\n"
-    else
-        seq 10000 10199 | awk '{print "104.25.158.178 finland"$1".discord.media"}' \
-            | grep -vxFf /etc/hosts >> /etc/hosts
-echo -e "\n${MAGENTA}Добавляем Финские IP${NC}"
-        /etc/init.d/dnsmasq restart 2>/dev/null
-echo -e "${GREEN}Финские IP добавлены${NC}\n"
-    fi
-    read -p "Нажмите Enter..." dummy
-}
-
-$output_shown && echo
-
+toggle_finland_hosts() { if grep -q "$pattern" /etc/hosts; then sed -i "/$pattern/d" /etc/hosts
+echo -e "\n${MAGENTA}Удаляем Финские IP${NC}"; /etc/init.d/dnsmasq restart 2>/dev/null; echo -e "${GREEN}Финские IP удалены${NC}\n"
+else seq 10000 10199 | awk '{print "104.25.158.178 finland"$1".discord.media"}' | grep -vxFf /etc/hosts >> /etc/hosts
+echo -e "\n${MAGENTA}Добавляем Финские IP${NC}"; /etc/init.d/dnsmasq restart 2>/dev/null; echo -e "${GREEN}Финские IP добавлены${NC}\n"
+fi; read -p "Нажмите Enter..." dummy; }; $output_shown && echo
 if [ "$NO_PAUSE" = "1" ]; then SELECTED="50-stun4all"; URL="https://raw.githubusercontent.com/bol-van/zapret/master/init.d/custom.d.examples.linux/50-stun4all"; else
 echo -e "${CYAN}1) ${GREEN}Установить скрипт ${NC}50-stun4all\n${CYAN}2) ${GREEN}Установить скрипт ${NC}50-quic4all\n${CYAN}3) ${GREEN}Установить скрипт ${NC}50-discord-media\n${CYAN}4) ${GREEN}Установить скрипт ${NC}50-discord\n${CYAN}5) ${GREEN}Удалить скрипт${NC}"
 grep -q '104\.25\.158\.178 finland[0-9]\{5\}\.discord\.media' /etc/hosts && FIN_TXT="${GREEN}Удалить Финские ${NC}IP ${GREEN}из ${NC}hosts" || FIN_TXT="${GREEN}Добавить Финские ${NC}IP ${GREEN}в ${NC}hosts"
