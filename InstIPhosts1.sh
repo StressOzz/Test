215ec2467542adce1a0841a855cef4772a6717a9
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-01-02 21:01:28 +0500

    Update and rename InstIPhost.sh to InstIPhosts1.sh

--- InstIPhosts1.sh
+++ InstIPhosts1.sh
@@ -2,6 +2,8 @@
 
 TOTAL=30
 FOUND_IPS=""
+
+# Заголовок
 printf "\033[36m=== Обновление IP Instagram ===\033[0m\n"
 printf "\033[32mЖдите, выполняется проверка %d раз…\033[0m\n\n" "$TOTAL"
 
@@ -20,22 +22,27 @@ printf "%s\n" "$PROGRESS"
 for i in $(seq 1 $TOTAL); do
     # Получаем IP
     IPs=$(dig @1.1.1.1 +https +short www.instagram.com | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u)
-    NEW_FOUND=0
+    
+    # Новые IP в этом прогоне
+    NEW_IP_FOUND=0
     for ip in $IPs; do
         echo "$FOUND_IPS" | grep -qx "$ip" && continue
         FOUND_IPS="$FOUND_IPS $ip"
         echo "$ip instagram.com www.instagram.com" >> /etc/hosts
         printf "\033[32mНовый IP найден: %s instagram.com www.instagram.com\033[0m\n" "$ip"
-        NEW_FOUND=1
+        NEW_IP_FOUND=1
     done
-    # Обновляем прогресс-бар
-    PROGRESS=$(echo "$PROGRESS" | sed "s/□/■/$i")
+    
+    # Закрашиваем один квадратик за этот прогон
+    PROGRESS=$(echo "$PROGRESS" | sed "s/□/■/1")
     printf "\r%s" "$PROGRESS"
     sleep 0.1
 done
 
-# Финальный вывод
+# Финальное применение DNS
 printf "\n"
 /etc/init.d/dnsmasq restart >/dev/null 2>&1
+
+# Завершение
 printf "\n\033[36m=== Готово! ===\033[0m\n"
 printf "\033[32mПерезапустите приложение и браузер и проверьте Instagram\033[0m\n"
