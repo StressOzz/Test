a9eddb1d89eab59752caae58b85ae710342417f3
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-01-14 20:14:21 +0500

    Update and rename test20.sh to test21.sh

--- test21.sh
+++ test21.sh
@@ -254,7 +254,7 @@ then if grep -q 'ct original packets ge 30 flow offload @ft;' /usr/share/firewal
 else echo -e "${CYAN}0) ${GREEN}Применить${NC} FIX ${GREEN}для${NC} Flow Offloading"; fi; fi
 echo -ne "${CYAN}Enter) ${GREEN}Выход в главное меню${NC}\n\n${YELLOW}Выберите пункт:${NC} " && read -r choiceMN; case "$choiceMN" in
 1) wget -q -U "Mozilla/5.0" -O - https://raw.githubusercontent.com/StressOzz/Zapret-Manager/refs/heads/main/sys_info.sh | sh; echo; read -p "Нажмите Enter..." dummy ;; 2) toggle_web ;; 3) toggle_quic ;;
-4) stop_zapret "1"; echo -e "${YELLOW}Прервать${NC} blockcheck${YELLOW} - ${NC}Ctrl+C"; echo; read -p "Нажмите Enter для запуска..." dummy
+4) stop_zapret "1"; echo -e "${YELLOW}Прервать${NC} blockcheck${YELLOW} и выйти в терминал - ${NC}Ctrl+C"; echo; read -p "Нажмите Enter для запуска..." dummy
 clear; echo -e "${MAGENTA}blockcheck${NC}\n"; chmod +x /opt/zapret/blockcheck.sh; /opt/zapret/blockcheck.sh; start_zapret ;;
 0) if uci get firewall.@defaults[0].flow_offloading 2>/dev/null | grep -q '^1$' || uci get firewall.@defaults[0].flow_offloading_hw 2>/dev/null | grep -q '^1$'
 then if grep -q 'ct original packets ge 30 flow offload @ft;' /usr/share/firewall4/templates/ruleset.uc; then echo -e "\n${MAGENTA}Отключаем FIX для Flow Offloading${NC}"
