e29f7273259db9cc467c24ab25f42c10e3576739
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-01-16 01:23:38 +0500

    Update and rename test46.sh to test47.sh

--- test47.sh
+++ test47.sh
@@ -96,7 +96,7 @@ backup_menu() { [ ! -f /etc/init.d/zapret ] && { echo -e "\n${RED}Zapret не у
 while true; do clear; echo -e "${MAGENTA}Меню управление настройками${NC}\n"; if [ -f "$DATE_FILE" ]; then CREATE_DATE=$(cat "$DATE_FILE")
 echo -e "${YELLOW}Резервная копия:${NC} $CREATE_DATE ($(du -sh /opt/zapret_backup 2>/dev/null | awk '{print $1}'))\n"; else echo -e "${YELLOW}Резервная копия: ${RED}отсутствует${NC}\n"; fi
 echo -e "${CYAN}1) ${GREEN}Сделать резервную копию настроек${NC} Zapret\n${CYAN}2) ${GREEN}Восстановить настройки ${NC}Zapret${GREEN} из резервной копии${NC}\n${CYAN}3) ${GREEN}Вернуть настройки по умолчанию${NC}"
-echo -ne "${CYAN}4) ${GREEN}Показать сохранённую стратегию${NC}\n${CYAN}5) ${GREEN}Удалить резервную копию${NC}\n${CYAN}Enter) ${GREEN}Выход в главное меню${NC}\n\n${YELLOW}Выберите пункт: ${NC}"
+echo -ne "${CYAN}4) ${GREEN}Показать стратегию из резервной копии${NC}\n${CYAN}5) ${GREEN}Удалить резервную копию${NC}\n${CYAN}Enter) ${GREEN}Выход в главное меню${NC}\n\n${YELLOW}Выберите пункт: ${NC}"
 read choice; case $choice in 1) save_backup ;; 2) restore_backup ;; 3) restore_default ;; 5) delete_backup ;;
 4) if [ -d "$BACKUP_DIR" ]; then echo; awk 'f{if($0=="'\''")exit;print} /option NFQWS_OPT '\''/{f=1}' /opt/zapret_backup/etc/config/zapret; echo; PAUSE; else echo -e "\n${RED}Резервная копия не найдена!${NC}\n"; PAUSE; fi ;; *) break ;; esac; done; }
 delete_backup() { if [ -d "$BACKUP_DIR" ]; then rm -rf "$BACKUP_DIR"; echo -e "\n${GREEN}Резервная копия удалена!${NC}\n"; else echo -e "\n${RED}Резервная копия не найдена!${NC}\n"; fi; PAUSE; }
