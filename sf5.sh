f306b7e0714341c7aa9b6c0d48e1a3fbadddb5ab
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-01-21 17:47:36 +0500

    Update and rename sf4.sh to sf5.sh

--- sf5.sh
+++ sf5.sh
@@ -7,21 +7,21 @@ ZIP="$TMP/repo.zip"
 mkdir -p "$TMP" || exit 1
 : > "$OUT"
 
-# качаем архив репозитория
 wget -qO "$ZIP" https://github.com/Flowseal/zapret-discord-youtube/archive/refs/heads/main.zip || exit 1
-
-# распаковываем
 unzip -oq "$ZIP" -d "$TMP" || exit 1
 
 BASE="$TMP/zapret-discord-youtube-main"
 
-# ищем ВСЕ файлы general*
 find "$BASE" -type f -name 'general*' | while read -r F; do
+  MATCH=$(grep -E \
+    '^--filter-udp=19294-19344,50000-50100|^--filter-tcp=2053,2083,2087,2096,8443|^--filter-tcp=443 --hostlist="%LISTS%list-google.txt"|^--filter-tcp=80,443 --hostlist="%LISTS%list-general.txt"' \
+    "$F")
+
+  [ -z "$MATCH" ] && continue
+
   {
     echo "# $(basename "$F")"
-    sed 's/--/\n--/g' "$F" \
-    | grep -E '^--filter-(udp|tcp)=' \
-    | grep -E '19294-19344,50000-50100|2053,2083,2087,2096,8443|list-google.txt|list-general.txt'
+    echo "$MATCH" | sed 's/--/\n--/g' | sed '/^$/d'
     echo
   } >> "$OUT"
 done
