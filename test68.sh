67a56c40c76ecc723e4efcf91f660691987b09ae
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-01-24 01:11:29 +0500

    Update and rename test67.sh to test68.sh

--- test68.sh
+++ test68.sh
@@ -344,10 +344,10 @@ add_block() { while IFS= read -r line; do [ -z "$line" ] && continue; grep -Fxq 
 remove_block() { while IFS= read -r line; do [ -z "$line" ] && continue; sed -i "\|^$line$|d" "$HOSTS_FILE"; done < <(printf '%s\n' "$1"); }
 toggle_block() { if status_block "$1"; then remove_block "$1"; echo -e "\n${CYAN}Удаляем и применяем${NC}"; else add_block "$1"; echo -e "\n${CYAN}Добавляем и применяем${NC}"; fi; /etc/init.d/dnsmasq restart >/dev/null 2>&1; echo -e "${GREEN}Готово!${NC}\n"; PAUSE; }
 toggle_all() { if status_block "$ALL_BLOCKS"; then remove_block "$ALL_BLOCKS"; echo -e "\n${CYAN}Удаляем и применяем${NC}"; else add_block "$ALL_BLOCKS"; echo -e "\n${CYAN}Добавляем и применяем${NC}"; fi; /etc/init.d/dnsmasq restart >/dev/null 2>&1; echo -e "${GREEN}Готово!${NC}\n"; PAUSE; }
-menu_hosts() { while true; do clear; status_block "$INSTAGRAM" && S1="${RED}Удалить" || S1="${GREEN}Добавить"; status_block "$PDA" && S2="${RED}Удалить" || S2="${GREEN}Добавить"; status_block "$NTC" && S3="${RED}Удалить" || S3="${GREEN}Добавить"
-status_block "$RUTOR" && S4="${RED}Удалить" || S4="${GREEN}Добавить"; status_block "$LIBRUSEC" && S5="${RED}Удалить" || S5="${GREEN}Добавить"; status_block "$ALL_BLOCKS" && S6="${RED}Удалить все" || S6="${GREEN}Добавить все"
-echo -e "${YELLOW}Меню редактирования /etc/hosts${NC}\n\n${CYAN}1) $S2 ${NC}IP ${GREEN}для${NC} 4Pda\n${CYAN}2) $S4 ${NC}IP ${GREEN}для${NC} Rutor\n${CYAN}3) $S3 ${NC}IP ${GREEN}для${NC} ntc.party\n${CYAN}4) $S1 ${NC}IP ${GREEN}для${NC} Instagram"
-echo -e "${CYAN}5) $S5 ${NC}IP ${GREEN}для${NC} Lib.rus.ec\n${CYAN}6) $S6 ${NC}IP\n${CYAN}Enter) ${GREEN}Выход в меню стратегий${NC}"
+menu_hosts() { while true; do clear; status_block "$INSTAGRAM" && S1="Удалить" || S1="Добавить"; status_block "$PDA" && S2="Удалить" || S2="Добавить"; status_block "$NTC" && S3="Удалить" || S3="Добавить"
+status_block "$RUTOR" && S4="Удалить" || S4="Добавить"; status_block "$LIBRUSEC" && S5="Удалить" || S5="Добавить"; status_block "$ALL_BLOCKS" && S6="Удалить все" || S6="Добавить все"
+echo -e "${YELLOW}Меню редактирования /etc/hosts${NC}\n\n${CYAN}1) ${GREEN}$S2 ${NC}IP ${GREEN}для${NC} 4Pda\n${CYAN}2) ${GREEN}$S4 ${NC}IP ${GREEN}для${NC} Rutor\n${CYAN}3) ${GREEN}$S3 ${NC}IP ${GREEN}для${NC} ntc.party\n${CYAN}4) ${GREEN}$S1 ${NC}IP ${GREEN}для${NC} Instagram"
+echo -e "${CYAN}5) ${GREEN}$S5 ${NC}IP ${GREEN}для${NC} Lib.rus.ec\n${CYAN}6) ${GREEN}$S6 ${NC}IP\n${CYAN}Enter) ${GREEN}Выход в меню стратегий${NC}"
 echo -ne "\n${YELLOW}Выберите пункт:${NC} "; read -r choiceIP; case "$choiceIP" in 4) toggle_block "$INSTAGRAM";; 1) toggle_block "$PDA";; 3) toggle_block "$NTC";; 2) toggle_block "$RUTOR";; 5) toggle_block "$LIBRUSEC";; 6) toggle_all;; *) break;; esac; done; }
 # ==========================================
 # Главное меню
