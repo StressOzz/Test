97ad6ea4861e5f67a84909b00a827087d7201b58
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-01-25 20:06:52 +0500

    Update and rename str3.sh to str4.sh

--- str4.sh
+++ str4.sh
@@ -8,19 +8,24 @@ RESULTS="/tmp/zapret_bench.txt"
 # очистим предыдущие результаты
 : > "$RESULTS"
 
-# читаем блоки стратегий от # до следующего #
-awk 'BEGIN{RS=""; FS="\n"} /^#/{print $0}' "$STR_FILE" | while read -r BLOCK; do
-    # убираем лишние пустые строки в начале/конце блока
-    STRAT_BLOCK=$(echo "$BLOCK" | sed '/^$/d')
+# делим файл на блоки стратегий (от # до следующего #)
+awk '
+  /^#/ { 
+    if (NR != 1) print block; 
+    block=$0; 
+    next 
+  } 
+  { block = block "\n" $0 } 
+  END { print block }
+' "$STR_FILE" | while IFS= read -r STRAT_BLOCK; do
+    # вставляем блок стратегии в конфиг между option NFQWS_OPT ' и '
+    sed -i "/option NFQWS_OPT '/,/^'/c\	option NFQWS_OPT '\n$STRAT_BLOCK\n'" "$CONF"
 
     echo -e "\nПрименяем стратегию:\n$STRAT_BLOCK"
 
-    # вставляем стратегию в конфиг между option NFQWS_OPT ' и '
-    sed -i "/option NFQWS_OPT '/,/^'/c\	option NFQWS_OPT '\n$STRAT_BLOCK\n'" "$CONF"
-
     # рестарт Zapret
     /etc/init.d/zapret restart >/dev/null 2>&1
-    sleep 1  # даём время подняться
+    sleep 5
 
     # проверка сайтов
     OK=0
@@ -33,12 +38,10 @@ awk 'BEGIN{RS=""; FS="\n"} /^#/{print $0}' "$STR_FILE" | while read -r BLOCK; do
     done
 
     # сохраняем результат
-    echo "$OK/$TOTAL $STRAT_BLOCK" >> "$RESULTS"
+    echo "$OK/$TOTAL" >> "$RESULTS"
     echo "Доступно: $OK/$TOTAL"
 done
 
 # вывод топ-5 лучших стратегий
 echo -e "\nТоп-5 лучших стратегий:"
-sort -rn "$RESULTS" | head -5 | while read RES STR; do
-    echo "$RES → $STR"
-done
+paste -d ' ' <(awk '/^#/{print $0}' "$STR_FILE") "$RESULTS" | sort -rn -k2 | head -5
