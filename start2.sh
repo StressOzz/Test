af69efc95b72841f7c51c26ff4fd43ead2ee497c
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-01-30 00:56:53 +0500

    Delete start2.sh

--- start2.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-OPENWRT_VER=$(grep '^DISTRIB_RELEASE=' /etc/openwrt_release | cut -d"'" -f2 | cut -d. -f1)
-if [ "$OPENWRT_VER" -lt 25 ]; then sh <(wget -O - https://raw.githubusercontent.com/StressOzz/Zapret-Manager/main/ZM_24.sh)
-else sh <(wget -O - https://raw.githubusercontent.com/StressOzz/Zapret-Manager/main/ZM_25.sh); fi
