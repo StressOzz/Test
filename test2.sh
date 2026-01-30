01828a010bda48e250459c5c5c065ac223c3f292
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-01-30 19:26:04 +0500

    Update and rename test2.sh to test3.sh

--- test2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-RAW="https://raw.githubusercontent.com/hyperion-cs/dpi-checkers/main/ru/tcp-16-20/index.html"
-
-curl -fsSL "$RAW" | \
-grep 'url:' | \
-sed -n 's/.*id: "\([^"]*\)".*url: "\([^"]*\)".*/\1|\2/p'
