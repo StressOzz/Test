83d3717c9d2e1502334a7d5c8b0538078e2552b5
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-02-17 19:35:30 +0500

    Update and rename GEN+ING3.sh to GEN+ING4.sh

--- GEN+ING4.sh
+++ GEN+ING4.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+
+GREEN="\033[1;32m"
+RED="\033[1;31m"
+CYAN="\033[1;36m"
+YELLOW="\033[1;33m"
+MAGENTA="\033[1;35m"
+NC="\033[0m"
+WHITE="\033[1;37m"
+BLUE="\033[0;34m"
+GRAY='\033[38;5;239m'
+DGRAY="\033[38;5;244m"
+
 CONF="/root/WARP.conf"
 IFNAME="AWG"
 
@@ -176,6 +188,168 @@ echo "Интерфейс создан. Перезапускаем сеть..."
 /etc/init.d/network restart
 
 echo "Готово."
+
+
+
+ echo -e "\n${MAGENTA}Установка Podkop${NC}"
+
+    REPO="https://api.github.com/repos/itdoginfo/podkop/releases/latest"
+    DOWNLOAD_DIR="/tmp/podkop"
+
+    PKG_IS_APK=0
+    command -v apk >/dev/null 2>&1 && PKG_IS_APK=1
+
+    rm -rf "$DOWNLOAD_DIR"
+    mkdir -p "$DOWNLOAD_DIR"
+
+    msg() {
+        if [ -n "$2" ]; then
+            printf "\033[32;1m%s \033[37;1m%s\033[0m\n" "$1" "$2"
+        else
+            printf "\033[32;1m%s\033[0m\n" "$1"
+        fi
+    }
+
+    pkg_is_installed () {
+        local pkg_name="$1"
+        if [ "$PKG_IS_APK" -eq 1 ]; then
+            apk list --installed | grep -q "$pkg_name"
+        else
+            opkg list-installed | grep -q "$pkg_name"
+        fi
+    }
+
+    pkg_remove() {
+        local pkg_name="$1"
+        msg "Удаляем" "$pkg_name..."
+        if [ "$PKG_IS_APK" -eq 1 ]; then
+            apk del "$pkg_name" >/dev/null 2>&1
+        else
+            opkg remove --force-depends "$pkg_name" >/dev/null 2>&1
+        fi
+    }
+
+    pkg_list_update() {
+        msg "Обновляем список пакетов..."
+        if [ "$PKG_IS_APK" -eq 1 ]; then
+            apk update >/dev/null 2>&1
+        else
+            opkg update >/dev/null 2>&1
+        fi
+    }
+
+    pkg_install() {
+        local pkg_file="$1"
+        msg "Устанавливаем" "$(basename "$pkg_file")"
+        if [ "$PKG_IS_APK" -eq 1 ]; then
+            apk add --allow-untrusted "$pkg_file" >/dev/null 2>&1
+        else
+            opkg install "$pkg_file" >/dev/null 2>&1
+        fi
+    }
+
+    # Проверка системы
+    MODEL=$(cat /tmp/sysinfo/model 2>/dev/null || echo "не определено")
+    AVAILABLE_SPACE=$(df /overlay | awk 'NR==2 {print $4}')
+    REQUIRED_SPACE=26000
+	
+[ "$AVAILABLE_SPACE" -lt "$REQUIRED_SPACE" ] && { 
+    msg "Недостаточно свободного места"
+    echo ""
+    read -p "Нажмите Enter..." dummy
+    return
+}
+
+nslookup google.com >/dev/null 2>&1 || { 
+    msg "DNS не работает"
+    echo ""
+    read -p "Нажмите Enter..." dummy
+    return
+}
+
+
+    if pkg_is_installed https-dns-proxy; then
+        msg "Обнаружен конфликтный пакет" "https-dns-proxy. Удаляем..."
+        pkg_remove luci-app-https-dns-proxy
+        pkg_remove https-dns-proxy
+        pkg_remove luci-i18n-https-dns-proxy*
+    fi
+
+    # Проверка sing-box
+    if pkg_is_installed "^sing-box"; then
+        sing_box_version=$(sing-box version | head -n 1 | awk '{print $3}')
+        required_version="1.12.4"
+        if [ "$(echo -e "$sing_box_version\n$required_version" | sort -V | head -n 1)" != "$required_version" ]; then
+            msg "sing-box устарел. Удаляем..."
+            service podkop stop >/dev/null 2>&1
+            pkg_remove sing-box
+        fi
+    fi
+
+    /usr/sbin/ntpd -q -p 194.190.168.1 -p 216.239.35.0 -p 216.239.35.4 -p 162.159.200.1 -p 162.159.200.123 >/dev/null 2>&1
+
+pkg_list_update || { 
+    msg "Не удалось обновить список пакетов"
+    echo ""
+    read -p "Нажмите Enter..." dummy
+    return
+}
+
+
+    # Шаблон скачивания
+    if [ "$PKG_IS_APK" -eq 1 ]; then
+        grep_url_pattern='https://[^"[:space:]]*\.apk'
+    else
+        grep_url_pattern='https://[^"[:space:]]*\.ipk'
+    fi
+
+    download_success=0
+    urls=$(wget -qO- "$REPO" 2>/dev/null | grep -o "$grep_url_pattern")
+    for url in $urls; do
+        filename=$(basename "$url")
+        filepath="$DOWNLOAD_DIR/$filename"
+        msg "Скачиваем" "$filename"
+        if wget -q -O "$filepath" "$url" >/dev/null 2>&1 && [ -s "$filepath" ]; then
+            download_success=1
+        else
+            msg "Ошибка скачивания" "$filename"
+        fi
+    done
+
+[ $download_success -eq 0 ] && { 
+    msg "Нет успешно скачанных пакетов"
+    echo ""
+    read -p "Нажмите Enter..." dummy
+    return
+}
+
+    # Установка пакетов
+    for pkg in podkop luci-app-podkop; do
+        file=$(ls "$DOWNLOAD_DIR" | grep "^$pkg" | head -n 1)
+        [ -n "$file" ] && pkg_install "$DOWNLOAD_DIR/$file"
+    done
+
+    # Русский интерфейс
+    ru=$(ls "$DOWNLOAD_DIR" | grep "luci-i18n-podkop-ru" | head -n 1)
+    if [ -n "$ru" ]; then
+        if pkg_is_installed luci-i18n-podkop-ru; then
+            msg "Обновляем русский язык..." "$ru"
+            pkg_remove luci-i18n-podkop* >/dev/null 2>&1
+            pkg_install "$DOWNLOAD_DIR/$ru"
+        else
+			pkg_install "$DOWNLOAD_DIR/$ru"
+
+        fi
+    fi
+
+    # Очистка
+    rm -rf "$DOWNLOAD_DIR"
+
+    echo -e "Podkop ${GREEN}успешно установлен!${NC}\n"
+    read -p "Нажмите Enter..." dummy
+}
+
+
 
 echo -e "Меняем конфигурацию в Podkop"
     # Создаём / меняем /etc/config/podkop
