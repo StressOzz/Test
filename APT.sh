bc582ede9d9da828349de49ec2774adec145ed3b
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-02-22 21:31:02 +0500

    Update APT.sh

--- APT.sh
+++ APT.sh
@@ -11,12 +11,12 @@ BLUE="\033[0;34m"
 GRAY='\033[38;5;239m'
 DGRAY="\033[38;5;244m"
 
-CONF="/root/WARP.conf"
-IFNAME="AWG"
-
-echo -e "\n${MAGENTA}Устанавливаем AWG + интерфейс${NC}"
+# ==========================================
+# Устанавливаем AWG + интерфейс
+# ==========================================
+echo -e "\n${MAGENTA}Устанавка AWG + интерфейс${NC}"
 echo -e "${GREEN}Обновляем список пакетов${NC}"
-opkg update >/dev/null 2>&1 || { echo -e "\n${RED}Ошибка при обновлении списка пакетов!${NC}\n"; exit; }
+opkg update >/dev/null 2>&1 || { echo -e "\n${RED}Ошибка при обновлении списка пакетов!${NC}\n"; read -p "Нажмите Enter..." dummy; return; }
 PKGARCH=$(opkg print-architecture | awk 'BEGIN {max=0} {if ($3 > max) {max = $3; arch = $2}} END {print arch}')
 TARGET=$(ubus call system board | jsonfilter -e '@.release.target' | cut -d '/' -f1)
 SUBTARGET=$(ubus call system board | jsonfilter -e '@.release.target' | cut -d '/' -f2)
@@ -33,11 +33,11 @@ local url="${BASE_URL}v${VERSION}/${filename}"
         echo -e "${CYAN}Устанавливаем ${NC}$pkgname"
         if ! opkg install "$AWG_DIR/$filename" >/dev/null 2>&1 ; then
             echo -e "\n${RED}Ошибка установки $pkgname!${NC}\n"
-           exit
+            read -p "Нажмите Enter..." dummy; return
         fi
     else
         echo -e "\n${RED}Ошибка! Не удалось скачать $filename${NC}\n"
-        exit
+        read -p "Нажмите Enter..." dummy; return
     fi
 }
 install_pkg "kmod-amneziawg"
@@ -45,9 +45,12 @@ install_pkg "amneziawg-tools"
 install_pkg "luci-proto-amneziawg"
 install_pkg "luci-i18n-amneziawg-ru" >/dev/null 2>&1 || echo -e "${RED}Внимание: русская локализация не установлена (не критично)${NC}"
 rm -rf "$AWG_DIR"
+echo -e "${YELLOW}Перезапускаем сеть! Подождите...${NC}"
+/etc/init.d/network restart >/dev/null 2>&1
+sleep 5
 echo -e "AmneziaWG ${GREEN}установлен!${NC}"
 
-echo -e "\n${MAGENTA}Устанавливаем интерфейс AWG${NC}"
+echo -e "${MAGENTA}Устанавливаем интерфейс AWG${NC}"
 IF_NAME="AWG"
 PROTO="amneziawg"
 DEV_NAME="amneziawg0"
@@ -62,9 +65,22 @@ set network.$IF_NAME.device=$DEV_NAME
 commit network
 EOF
 fi
-echo -e "${GREEN}Интерфейс ${NC}$IF_NAME${GREEN} создан!${NC}"
+echo -e "${CYAN}Перезапускаем сеть${NC}"
 
-echo -en "\n${YELLOW}Вставьте рабочий конфиг в Interfaces (Интерфейс) AWG в LuCI и нажмите Save затем Save & Apply!\nНажмите Enter для установки Podkop!${NC}"; read -p "" dummy
+/etc/init.d/network restart
+sleep 5
+# /etc/init.d/firewall restart
+# /etc/init.d/uhttpd restart
+
+echo -e "${GREEN}Интерфейс ${NC}$IF_NAME${GREEN} создан и активирован!${NC}"
+echo -e "${YELLOW}Вставьте рабочий конфиг в Interfaces (Интерфейс) AWG!${NC}\n"
+read -p "Нажмите Enter..." dummy
+
+
+# ==========================================
+# Установка Podkop
+# ==========================================
+
 
 echo -e "\n${MAGENTA}Установка Podkop${NC}"
 
@@ -96,7 +112,7 @@ echo -e "\n${MAGENTA}Установка Podkop${NC}"
 
     pkg_remove() {
         local pkg_name="$1"
-echo -e "${CYAN}Удаляем" "$pkg_name...${NC}"
+        msg "Удаляем" "$pkg_name..."
         if [ "$PKG_IS_APK" -eq 1 ]; then
             apk del "$pkg_name" >/dev/null 2>&1
         else
@@ -105,7 +121,7 @@ echo -e "${CYAN}Удаляем" "$pkg_name...${NC}"
     }
 
     pkg_list_update() {
-echo -e "${CYAN}Обновляем список пакетов...${NC}"
+        msg "Обновляем список пакетов..."
         if [ "$PKG_IS_APK" -eq 1 ]; then
             apk update >/dev/null 2>&1
         else
@@ -115,7 +131,7 @@ echo -e "${CYAN}Обновляем список пакетов...${NC}"
 
     pkg_install() {
         local pkg_file="$1"
-echo -e "${CYAN}Устанавливаем" "$(basename "$pkg_file")${NC}"
+        msg "Устанавливаем" "$(basename "$pkg_file")"
         if [ "$PKG_IS_APK" -eq 1 ]; then
             apk add --allow-untrusted "$pkg_file" >/dev/null 2>&1
         else
@@ -123,32 +139,39 @@ echo -e "${CYAN}Устанавливаем" "$(basename "$pkg_file")${NC}"
         fi
     }
 
+    # Проверка системы
     MODEL=$(cat /tmp/sysinfo/model 2>/dev/null || echo "не определено")
     AVAILABLE_SPACE=$(df /overlay | awk 'NR==2 {print $4}')
     REQUIRED_SPACE=26000
 	
 [ "$AVAILABLE_SPACE" -lt "$REQUIRED_SPACE" ] && { 
-echo -e "${CYAN}Недостаточно свободного места${NC}"
-    exit
+    msg "Недостаточно свободного места"
+    echo ""
+    read -p "Нажмите Enter..." dummy
+    return
 }
 
 nslookup google.com >/dev/null 2>&1 || { 
-echo -e "${GREEN}DNS не работает${NC}"
-	exit
+    msg "DNS не работает"
+    echo ""
+    read -p "Нажмите Enter..." dummy
+    return
 }
 
+
     if pkg_is_installed https-dns-proxy; then
-echo -e "${CYAN}Обнаружен конфликтный пакет" "https-dns-proxy. Удаляем...${NC}"
+        msg "Обнаружен конфликтный пакет" "https-dns-proxy. Удаляем..."
         pkg_remove luci-app-https-dns-proxy
         pkg_remove https-dns-proxy
         pkg_remove luci-i18n-https-dns-proxy*
     fi
 
+    # Проверка sing-box
     if pkg_is_installed "^sing-box"; then
         sing_box_version=$(sing-box version | head -n 1 | awk '{print $3}')
         required_version="1.12.4"
         if [ "$(echo -e "$sing_box_version\n$required_version" | sort -V | head -n 1)" != "$required_version" ]; then
-echo -e "${CYAN}sing-box устарел. Удаляем...${NC}"
+            msg "sing-box устарел. Удаляем..."
             service podkop stop >/dev/null 2>&1
             pkg_remove sing-box
         fi
@@ -157,10 +180,14 @@ echo -e "${CYAN}sing-box устарел. Удаляем...${NC}"
     /usr/sbin/ntpd -q -p 194.190.168.1 -p 216.239.35.0 -p 216.239.35.4 -p 162.159.200.1 -p 162.159.200.123 >/dev/null 2>&1
 
 pkg_list_update || { 
-echo -e "${CYAN}Не удалось обновить список пакетов${NC}"
-	exit
+    msg "Не удалось обновить список пакетов"
+    echo ""
+    read -p "Нажмите Enter..." dummy
+    return
 }
 
+
+    # Шаблон скачивания
     if [ "$PKG_IS_APK" -eq 1 ]; then
         grep_url_pattern='https://[^"[:space:]]*\.apk'
     else
@@ -172,28 +199,32 @@ echo -e "${CYAN}Не удалось обновить список пакетов
     for url in $urls; do
         filename=$(basename "$url")
         filepath="$DOWNLOAD_DIR/$filename"
-echo -e "${GREEN}Скачиваем" "$filename${NC}"
+        msg "Скачиваем" "$filename"
         if wget -q -O "$filepath" "$url" >/dev/null 2>&1 && [ -s "$filepath" ]; then
             download_success=1
         else
-echo -e "${RED}Ошибка скачивания" "$filename${NC}"
+            msg "Ошибка скачивания" "$filename"
         fi
     done
 
 [ $download_success -eq 0 ] && { 
-echo -e "${RED}Нет успешно скачанных пакетов${NC}"
-	exit
+    msg "Нет успешно скачанных пакетов"
+    echo ""
+    read -p "Нажмите Enter..." dummy
+    return
 }
 
+    # Установка пакетов
     for pkg in podkop luci-app-podkop; do
         file=$(ls "$DOWNLOAD_DIR" | grep "^$pkg" | head -n 1)
         [ -n "$file" ] && pkg_install "$DOWNLOAD_DIR/$file"
     done
 
+    # Русский интерфейс
     ru=$(ls "$DOWNLOAD_DIR" | grep "luci-i18n-podkop-ru" | head -n 1)
     if [ -n "$ru" ]; then
         if pkg_is_installed luci-i18n-podkop-ru; then
-echo -e "${CYAN}Обновляем русский язык...${NC}" "$ru"
+            msg "Обновляем русский язык..." "$ru"
             pkg_remove luci-i18n-podkop* >/dev/null 2>&1
             pkg_install "$DOWNLOAD_DIR/$ru"
         else
@@ -202,14 +233,19 @@ echo -e "${CYAN}Обновляем русский язык...${NC}" "$ru"
         fi
     fi
 
+    # Очистка
     rm -rf "$DOWNLOAD_DIR"
 
     echo -e "Podkop ${GREEN}успешно установлен!${NC}\n"
-    sleep 10
+    read -p "Нажмите Enter..." dummy
 
+# ==========================================
+# Интеграция AWG
+# ==========================================
+echo -e "\n${MAGENTA}Интегрируем AWG в Podkop${NC}"
 
-
-echo -e "${MAGENTA}Меняем конфигурацию в Podkop${NC}"
+echo -e "${GREEN}Меняем конфигурацию в ${NC}Podkop${GREEN}...${NC}"
+    # Создаём / меняем /etc/config/podkop
     cat <<EOF >/etc/config/podkop
 config settings 'settings'
 	option dns_type 'udp'
@@ -226,7 +262,6 @@ config settings 'settings'
 	option dont_touch_dhcp '0'
 	option config_path '/etc/sing-box/config.json'
 	option cache_path '/tmp/sing-box/cache.db'
-	option log_level 'warn'
 	option exclude_ntp '0'
 	option shutdown_correctly '0'
 
@@ -237,13 +272,19 @@ config section 'main'
 	option user_domain_list_type 'disabled'
 	option user_subnet_list_type 'disabled'
 	option mixed_proxy_enabled '0'
-	list community_lists 'telegram'
+	list community_lists 'russia_inside'
+	list community_lists 'hodca'
 EOF
 
 echo -e "AWG ${GREEN}интегрирован в ${NC}Podkop${GREEN}.${NC}"
 echo -e "${CYAN}Запускаем ${NC}Podkop${NC}"
+podkop enable >/dev/null 2>&1
+echo -e "${CYAN}Применяем конфигурацию${NC}"
+podkop reload >/dev/null 2>&1
 podkop restart >/dev/null 2>&1
-sleep 10
+echo -e "${CYAN}Обновляем списки${NC}"
+podkop list_update >/dev/null 2>&1
+echo -e "${CYAN}Перезапускаем сервис${NC}"
+podkop restart >/dev/null 2>&1
 echo -e "Podkop ${GREEN}готов к работе!${NC}\n"
-
-echo -e "${GREEN}Готово!${NC}"
+read -p "Нажмите Enter..." dummy
