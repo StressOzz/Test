e4d5f35682fb13f47101954c1a59ef49a8193f85
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-02-28 18:14:48 +0500

    Update and rename t4.sh to t5.sh

--- t5.sh
+++ t5.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
+clear 
+echo -e "Удаляем..."
 
-# СТОП
 killall mihomo hev-socks5-tunnel magitrickled 2>/dev/null
 for svc in mihomo hev-socks5-tunnel magitrickle; do rm -f /etc/init.d/$svc /etc/rc.d/S99$svc; done
 
-# ПАКЕТЫ С --FORCE
 opkg remove --force-removal-of-dependent-packages hev-socks5-tunnel magitrickle kmod-nft-tproxy >/dev/null 2>&1
 
-# rm ВСЁ СИЛЬНО
 rm -rf /etc/{mihomo,magitrickle,"hev-socks5-tunnel"} /etc/config/{hev*,magitri*,mihom*}
 rm -f /usr/bin/{mihomo,hev*,magitri*}
 rm -rf /www/luci-static/resources/view/{mihomo*,magitri*,hev*}
@@ -17,5 +16,5 @@ rm -rf /usr/lib/opkg/info/*{hev,magitri,mihom,kmod-nft}* /tmp/luci-*
 /etc/init.d/rpcd restart >/dev/null 2>&1
 /etc/init.d/uhttpd restart >/dev/null 2>&1
 
-echo "✅ ЧИСТО! reboot? (y)"
+echo -e "\n\n Удаление завершино! reboot? (y)"
 read ans; [ "$ans" = y ] && reboot
