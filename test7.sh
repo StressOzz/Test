db71b98b2d22f18380b28f5853e291f632b4d2be
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-03-01 20:25:52 +0500

    Update and rename test6.sh to test7.sh

--- test7.sh
+++ test7.sh
@@ -65,43 +65,67 @@ END{
   }
   split_endpoint(endpoint)
 
-  if(allowed=="") allowed="0.0.0.0/0,::/0"
-  n=split(allowed, aip, ",")
-  allowed_yaml="["
-  for(i=1;i<=n;i++){
-    if(i>1) allowed_yaml=allowed_yaml ", "
-    allowed_yaml=allowed_yaml "\"" aip[i] "\""
-  }
-  allowed_yaml=allowed_yaml "]"
+  # Сайт у вас почему-то кладет только IPv4 default-route в allowed-ips.
+  # Возьмем из WARP.conf если есть, иначе 0.0.0.0/0.
+  if(allowed=="") allowed="0.0.0.0/0"
+
+  # В примере с сайта ip без /32. Уберем суффикс /32 если он есть.
+  ip_no_mask=addr4
+  sub(/\/32$/, "", ip_no_mask)
 
   print "mixed-port: 7890" > OUT
-  print "allow-lan: true" >> OUT
+  print "allow-lan: false" >> OUT
+  print "tcp-concurrent: true" >> OUT
   print "mode: rule" >> OUT
   print "log-level: info" >> OUT
+  print "ipv6: false" >> OUT
+  print "external-controller: 0.0.0.0:9090" >> OUT
+  print "external-ui: ui" >> OUT
+  print "external-ui-url: https://github.com/MetaCubeX/metacubexd/releases/latest/download/compressed-dist.tgz" >> OUT
+  print "secret: \"\"" >> OUT
+  print "unified-delay: true" >> OUT
+  print "profile:" >> OUT
+  print "  store-selected: true" >> OUT
+  print "  store-fake-ip: true" >> OUT
+  print "" >> OUT
+
+  print "proxy-groups:" >> OUT
+  print "  - name: GLOBAL" >> OUT
+  print "    type: select" >> OUT
+  print "    proxies:" >> OUT
+  print "      - WARP" >> OUT
+  print "      - REJECT" >> OUT
+  print "" >> OUT
+
+  print "rules:" >> OUT
+  print "  - \"MATCH,GLOBAL\"" >> OUT
   print "" >> OUT
 
   print "proxies:" >> OUT
-  print "  - name: \"WARP\"" >> OUT
+  print "  - name: WARP" >> OUT
   print "    type: wireguard" >> OUT
   print "    server: " host >> OUT
   if(port!="") print "    port: " port >> OUT
-  if(addr4!="") print "    ip: " addr4 >> OUT
-  if(addr6!="") print "    ipv6: " addr6 >> OUT
   print "    private-key: \"" priv "\"" >> OUT
+  print "    udp: true" >> OUT
+  if(ip_no_mask!="") print "    ip: " ip_no_mask >> OUT
+  if(addr6!="") {
+    ipv6_no_mask=addr6
+    sub(/\/128$/, "", ipv6_no_mask)
+    print "    ipv6: " ipv6_no_mask >> OUT
+  }
   print "    public-key: \"" pub "\"" >> OUT
   if(psk!="") print "    pre-shared-key: \"" psk "\"" >> OUT
-  print "    allowed-ips: " allowed_yaml >> OUT
-  if(mtu!="") print "    mtu: " mtu >> OUT
+  print "    allowed-ips:" >> OUT
+  n=split(allowed, aip, ",")
+  for(i=1;i<=n;i++){
+    # выкинем ::/0 если вдруг есть, чтобы совпасть с вашим примером с сайта
+    if(aip[i]=="::/0") continue
+    print "      - \"" aip[i] "\"" >> OUT
+  }
   if(keep!="") print "    persistent-keepalive: " keep >> OUT
-
-  print "" >> OUT
-  print "proxy-groups:" >> OUT
-  print "  - name: \"PROXY\"" >> OUT
-  print "    type: select" >> OUT
-  print "    proxies: [\"WARP\", \"DIRECT\"]" >> OUT
-  print "" >> OUT
-  print "rules:" >> OUT
-  print "  - MATCH,PROXY" >> OUT
+  if(mtu!="") print "    mtu: " mtu >> OUT
+  print "    ip-version: ipv4" >> OUT
 }
 ' "$IN"
 
