f787e31e7c6f2b2924e4b6f7c8d15bde693bd497
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-03-03 21:49:06 +0500

    Update in.sh

--- in.sh
+++ in.sh
@@ -8,9 +8,45 @@ MAGENTA="\033[1;35m"
 BLUE="\033[0;34m"
 NC="\033[0m"
 DGRAY="\033[38;5;244m"
+CONFIGPATH="/etc/magitrickle/state/config.yaml"
+URL_DEFAULT="https://raw.githubusercontent.com/StressOzz/Use_WARP_on_OpenWRT/refs/heads/main/files/MagiTrickle/config.yaml"
+URL_ITDOG="https://raw.githubusercontent.com/StressOzz/Use_WARP_on_OpenWRT/refs/heads/main/files/MagiTrickle/configAD.yaml"
+
 PAUSE() { echo -ne "Нажмите Enter..."; read dummy; }
 
+magitrickle_config() {
+echo
+echo -e "Выбор списка для MagiTrickle"
+echo -e "1) ITDog Allow Domains"
+echo -e "2) Internet Helper"
+echo -e "3) Oставить текущий список"
+echo
 
+  while true; do
+    echo -en "Введите номер: "
+    read -r choice
+    choice="${choice:-2}"
+
+    case "$choice" in
+      1) MAGITRICKLE_CONFIG_URL="$URL_ITDOG"; return 0 ;;
+      2) MAGITRICKLE_CONFIG_URL="$URL_DEFAULT"; return 0 ;;
+      3) MAGITRICKLE_CONFIG_URL=""; return 0 ;;
+      *) echo "Неверный выбор. Введите 1, 2 или 3." ;;
+    esac
+  done
+
+if [ -n "$MAGITRICKLE_CONFIG_URL" ]; then
+  wget -q -O "$CONFIGPATH" "$MAGITRICKLE_CONFIG_URL" || {
+    echo "Ошибка: не удалось скачать список!"
+    echo "URL: $MAGITRICKLE_CONFIG_URL"
+    return 1
+  }
+
+  if [ ! -s "$CONFIGPATH" ]; then
+  echo; echo "Ошибка: config пустой/не создан: $CONFIGPATH"; echo
+  fi
+fi
+}
 
 show_menu() {
 clear
@@ -26,15 +62,17 @@ echo -e "4) Сгененрировать WARP"
 echo -e "5) Интегрировать WARP в mixomo-openwrt"
 echo -e "6) Удалить → установить → настроить mixomo-openwrt"
 echo -e "Enter) Выход"
-echo -ne "Выберите пункт: " && read choice
+echo -ne "Выберите пункт: " && read choiceM
 
-case "$choice" in
-1) $Z_ACTION_FUNC;;
-2) start_zapret;;
-3) menu_str;;
-4) DoH_menu;;
-5) Discord_menu;;
-0) sys_menu;;
+case "$choiceM" in
+1) sh <(wget -O - https://raw.githubusercontent.com/StressOzz/WARP_on_OpenWRT/main/mixomo_openwrt_install.sh); PAUSE;;
+2) sh <(wget -O - https://raw.githubusercontent.com/StressOzz/WARP_on_OpenWRT/main/mixomo_openwrt_delete.sh); PAUSE;;
+3) magitrickle_config; PAUSE;;
+4) sh <(wget -O - https://raw.githubusercontent.com/StressOzz/WARP_on_OpenWRT/main/gen_WARP.sh); PAUSE;;
+5) sh <(wget -O - https://raw.githubusercontent.com/StressOzz/WARP_on_OpenWRT/main/WARP_to_conf.sh); PAUSE;;
+0) sh <(wget -O - https://raw.githubusercontent.com/StressOzz/WARP_on_OpenWRT/main/mixomo_openwrt_install.sh)
+   sh <(wget -O - https://raw.githubusercontent.com/StressOzz/WARP_on_OpenWRT/main/gen_WARP.sh)
+   sh <(wget -O - https://raw.githubusercontent.com/StressOzz/WARP_on_OpenWRT/main/WARP_to_conf.sh); PAUSE;;
 *) echo; exit 0;;
 }
 while true; do menu; done
