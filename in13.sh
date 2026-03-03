90f4565896265df1b23b3eae30572341ea4da98c
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-03-03 23:56:43 +0500

    Update and rename in12.sh to in13.sh

--- in13.sh
+++ in13.sh
@@ -27,7 +27,6 @@ echo
 while true; do
   echo -en "${YELLOW}Введите номер: ${NC}"
   read -r choice
-  choice="${choice:-2}"
 
   case "$choice" in
     1) MAGITRICKLE_CONFIG_URL="$URL_ITDOG"; break ;;
@@ -87,7 +86,7 @@ echo -e "${CYAN}1) ${GREEN}Установить ${NC}mihomo-openwrt"
 echo -e "${CYAN}2) ${GREEN}Удалить ${NC}mihomo-openwrt"
 echo -e "${CYAN}3) ${GREEN}Сменить список ${NC}MagiTrickle"
 echo -e "${CYAN}4) ${GREEN}Сгенерировать ${NC}WARP"
-echo -e "${CYAN}5) ${GREEN}Интегрировать ${NC}WARP${GREEN} в ${NC}mihomo-openwrt"
+echo -e "${CYAN}5) ${GREEN}Интегрировать ${NC}/root/WARP.conf${GREEN} в ${NC}mihomo-openwrt"
 echo -e "${CYAN}6) ${GREEN}Удалить ${NC}→ ${GREEN}установить ${NC}→ ${GREEN}настроить ${NC}mihomo-openwrt"
 echo -e "${CYAN}Enter) ${GREEN}Выход"
 echo
