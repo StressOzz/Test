2da9e5d03ff6970bf827108c2a4d89c7ee4b0137
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-03-18 19:34:02 +0500

    Update and rename test4.sh to test5.sh

--- test5.sh
+++ test5.sh
@@ -32,7 +32,7 @@ while IFS='|' read -r country ep; do
 (
 host="${ep%%:*}"
 
-ping_ms="$(ping -c1 -W1 "$host" 2>/dev/null | awk -F'/' 'END{print int($5)}')"
+ping_ms="$(ping -c3 -W2 "$host" 2>/dev/null | awk -F'/' 'END{print int($5)}')"
 
 if [ -z "$ping_ms" ] || [ "$ping_ms" -eq 0 ]; then
 ping_val="FAIL"
