151519fdfba38c2921d12ac7c6654a1ba4f7f1fb
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-03-29 18:57:34 +0500

    Update and rename test1.sh to test2.sh

--- test2.sh
+++ test2.sh
@@ -11,7 +11,7 @@ NC="\033[0m"
 
 failed=0
 
-TG_URL="https://github.com/StressOzz/Test/raw/refs/heads/main/tg-ws-proxy-1.3.0.zip"
+TG_URL="https://github.com/StressOzz/Test/raw/refs/heads/main/tg-ws-proxy-main.zip"
 # TG_URL="https://github.com/Flowseal/tg-ws-proxy/archive/refs/heads/master.zip"
 
 ARCH="$(awk -F\' '/DISTRIB_ARCH/ {print $2}' /etc/openwrt_release)"
