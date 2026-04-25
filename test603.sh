9370e617b8eaa5f35fe248cbbd12ffd1a7dd1906
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-04-26 03:09:48 +0500

    Update and rename test602.sh to test603.sh

--- test603.sh
+++ test603.sh
@@ -598,9 +598,9 @@ echo -e "${CYAN}Enter) ${GREEN}Выход в главное меню${NC}\n"; ec
 # PODKOP EVOLUTION и AWG
 # ==========================================
 pkg_is_installed () { local pkg_name="$1"; if [ "$PKG_IS_APK" -eq 1 ]; then apk info -e "$pkg_name" >/dev/null 2>&1; else opkg list-installed | grep -q "^$pkg_name"; fi }
-PODKOP_VER() { PODKOP_LATEST_VER="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/yandexru45/podkop-evolution/releases/latest | sed 's#.*/tag/##')"; if command -v podkop >/dev/null 2>&1
-then LOCALPOD=$(podkop show_version 2>/dev/null | cut -d'-' -f1); LOCALPOD=${LOCALPOD#v}; [ -z "$LOCALPOD" ] && LOCALPOD="unknown"; else LOCALPOD="not_installed"; fi; if [ "$LOCALPOD" = "not_installed" ]
-then PODKOP_STATUS="${RED}не установлена${NC}"; elif [ "$PODKOP_LATEST_VER" = "unknown" ]; then PODKOP_STATUS="${GREEN}$LOCALPOD${NC}"; elif [ "$LOCALPOD" = "$PODKOP_LATEST_VER" ]; then PODKOP_STATUS="${GREEN}$LOCALPOD${NC}"; else PODKOP_STATUS="${RED}$LOCALPOD${NC}"; fi }
+
+PODKOP_VER() { PODKOP_LATEST_VER="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/yandexru45/podkop-evolution/releases/latest | sed 's#.*/tag/##')"; if ! command -v podkop >/dev/null 2>&1; then PODKOP_STATUS="${RED}не установлена${NC}"; return; fi; LOCALPOD="$(podkop show_version 2>/dev/null | cut -d'-' -f1 | sed 's/^v//')"; if [ "$PODKOP_LATEST_VER" = "$LOCALPOD" ]; then PODKOP_STATUS="${GREEN}$LOCALPOD${NC}"; else PODKOP_STATUS="${RED}$LOCALPOD${NC}"; fi }
+
 # УСТАНОВКА PODKOP
 PODKOP_INSTALL() { if ! pkg_is_installed podkop; then rm -rf "$tmpDIR"; mkdir -p "$tmpDIR"; echo -e "\n${MAGENTA}Устанавливаем Podkop Evolution${NC}"; echo -e "${CYAN}Обновляем список пакетов${NC}"; $UPDATE >/dev/null 2>&1 || { echo -e "\n${RED}Не удалось обновить список пакетов${NC}\n"; PAUSE; return; }
 PODKOP_LATEST_VER="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/yandexru45/podkop-evolution/releases/latest | sed 's#.*/tag/##')"; PODKOP_INST="https://github.com/yandexru45/podkop-evolution/releases/download/$PODKOP_LATEST_VER/podkop-$PODKOP_LATEST_VER-$VER_SUF.$APK_RAS"
