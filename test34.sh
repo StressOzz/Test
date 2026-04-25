f91aaa18359e022daabb94defaf18a687a7a5885
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-04-26 01:54:08 +0500

    Update and rename test33.sh to test34.sh

--- test34.sh
+++ test34.sh
@@ -36,14 +36,13 @@ PROTO="amneziawg"
 DEV_NAME="amneziawg0"
 BASE_URL="https://github.com/Slava-Shchipunov/awg-openwrt/releases/download/"
 tmpDIR="/tmp/PodkopAWG"
-
 pkg_is_installed () {
-    local pkg_name="$1"
-    if [ "$PKG_IS_APK" -eq 1 ]; then
-        apk info -e "$pkg_name" >/dev/null 2>&1
-    else
-        opkg list-installed | grep -q "^$pkg_name"
-    fi
+local pkg_name="$1"
+if [ "$PKG_IS_APK" -eq 1 ]; then
+apk info -e "$pkg_name" >/dev/null 2>&1
+else
+opkg list-installed | grep -q "^$pkg_name"
+fi
 }
 PODKOP_VER() {
 PODKOP_LATEST_VER="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/yandexru45/podkop-evolution/releases/latest | sed 's#.*/tag/##')"
@@ -93,8 +92,7 @@ echo -e "\n${MAGENTA}Удаление Podkop${NC}"
 $DELETE luci-i18n-podkop-ru >/dev/null 2>&1
 $DELETE luci-app-podkop >/dev/null 2>&1
 $DELETE podkop >/dev/null 2>&1
-rm -rf /etc/config/podkop >/dev/null 2>&1
-rm -f /etc/config/*podkop* >/dev/null 2>&1
+rm -rf /etc/config/podkop* /usr/bin/podkop >/dev/null 2>&1
 echo -e "Podkop ${GREEN}удалён!${NC}\n"
 PAUSE
 fi
@@ -185,28 +183,19 @@ echo -e "${NC}Network ${GREEN}→${NC} Interfaces ${GREEN}→${NC} AWG ${GREEN}�
 PAUSE
 }
 
-# ==========================================
 # Интеграция VPN
-# ==========================================
+
 PODKOP_VPN() {
-
 if ! pkg_is_installed podkop; then echo -e "\n${RED}Podkop Evolution не установлен!${NC}\n"; PAUSE; return; fi
-
-
 echo -e "\n${MAGENTA}Интегрируем VPN подписку в Podkop Evolution${NC}"
 echo -ne "\n${YELLOW}Введите ссылку на подписку (${NC}https://...${YELLOW}): ${NC}"
 read -r SUB_URL
-
 SUB_URL="$(echo "$SUB_URL" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')"
 echo "$SUB_URL" | grep -qE '^https?://' || { echo -e "\n${RED}Ошибка ссылки! Только ${NC}http://${RED} или ${NC}https://${RED} ссылки${NC}\n"; PAUSE; return; }
-
-
-
 echo -e "\n${CYAN}Меняем конфигурацию в ${NC}Podkop Evolution${NC}"
 
 printf "%s\n" "config settings 'settings'" "option dns_type 'udp'" "option dns_server '8.8.8.8'" "option bootstrap_dns_server '77.88.8.8'" "option dns_rewrite_ttl '60'" "list source_network_interfaces 'br-lan'" "option enable_output_network_interface '0'" "option enable_badwan_interface_monitoring '0'" "option enable_yacd '0'" "option disable_quic '0'" "option update_interval '1d'" "option download_lists_via_proxy '0'" "option dont_touch_dhcp '0'" "option config_path '/etc/sing-box/config.json'" "option cache_path '/tmp/sing-box/cache.db'" "option log_level 'panic'" "option exclude_ntp '0'" "option shutdown_correctly '0'" > /etc/config/podkop
 printf "%s\n" "" "config section 'StressKVN'" "option connection_type 'proxy'" "option proxy_config_type 'subscription'" "option enable_udp_over_tcp '0'" "option subscription_url '$SUB_URL'" "option subscription_update_interval '1h'" "option subscription_group_by_countries '0'" "option urltest_check_interval '5m'" "option urltest_tolerance '150'" "option urltest_testing_url 'https://www.gstatic.com/generate_204'" "list community_lists 'geoblock'" "list community_lists 'block'" "list community_lists 'porn'" "list community_lists 'news'" "list community_lists 'anime'" "list community_lists 'youtube'" "list community_lists 'discord'" "list community_lists 'meta'" "list community_lists 'twitter'" "list community_lists 'hdrezka'" "list community_lists 'tiktok'" "list community_lists 'telegram'" "list community_lists 'cloudflare'" "list community_lists 'google_ai'" "list community_lists 'google_play'" "list community_lists 'hodca'" "list community_lists 'roblox'" "list community_lists 'hetzner'" "list community_lists 'ovh'" "list community_lists 'digitalocean'" "list community_lists 'cloudfront'" "option user_domain_list_type 'disabled'" "option user_subnet_list_type 'disabled'" "option mixed_proxy_enabled '0'" >> /etc/config/podkop
-
 
 echo -e "${CYAN}Запускаем ${NC}Podkop Evolution${NC}"
 podkop enable >/dev/null 2>&1
@@ -217,17 +206,13 @@ podkop restart >/dev/null 2>&1
 echo -e "VPN подписка ${GREEN}интегрирована в ${NC}Podkop Evolution${GREEN}!${NC}\n"
 PAUSE
 }
-
-# ==========================================
 # МЕНЮ
-# ==========================================
 PODKOP_menu() { while true; do
 openwrt_version=$(cat /etc/openwrt_release | grep DISTRIB_RELEASE | cut -d"'" -f2 | cut -d'.' -f1)
 if [ "$openwrt_version" = "23" ]; then
 echo -e "\n${RED}OpenWrt версии ниже 24 не поддерживаются!${NC}\n"
 PAUSE; return
 fi
-
 AVAILABLE_SPACE=$(df /overlay 2>/dev/null | awk 'NR==2 {print $4}')
 [ -z "$AVAILABLE_SPACE" ] && AVAILABLE_SPACE=$(df / 2>/dev/null | awk 'NR==2 {print $4}')
 REQUIRED_SPACE=16000
@@ -257,7 +242,6 @@ echo -e "${YELLOW}Интерфейс AWG: ${GREEN}установлен${NC}"
 else
 echo -e "${YELLOW}Интерфейс AWG: ${RED}не установлен${NC}"
 fi
-
 if pkg_is_installed podkop; then
 echo -e "\n${CYAN}1) ${GREEN}Удалить ${NC}Podkop Evolution"; else
 echo -e "\n${CYAN}1) ${GREEN}Установить ${NC}Podkop Evolution"; fi
@@ -274,7 +258,7 @@ echo -e "${CYAN}Enter) ${GREEN}Выход в главное меню${NC}"
 echo -ne "\n${YELLOW}Выберите пункт:${NC} "
 read choicePOD
 case "$choicePOD" in
-1)  PODKOP_INSTALL ;;
+1) PODKOP_INSTALL ;;
 2) install_AWG ;;
 3) PODKOP_VPN ;;
 4) integration_AWG ;;
