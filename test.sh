af5d426f2d880ca428f1fc195796a582022afc87
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-05-16 13:46:39 +0500

    Update and rename test1.sh to test.sh

---
+++ test.sh
@@ -0,0 +1,144 @@
+#!/bin/sh
+
+CONF="/root/WARP.conf"
+IFACE="AWG"
+PEER="amneziawg_$IFACE"
+
+[ ! -f "$CONF" ] && {
+	echo "Файл $CONF не найден"
+	exit 1
+}
+
+# удалить старый интерфейс
+uci -q delete network.$IFACE
+uci -q delete network.$PEER
+
+# получить значение параметра
+get_val() {
+	sed -n "s/^$1 *= *//p" "$CONF" | head -n1
+}
+
+# добавить option если значение существует
+set_opt() {
+	local key="$1"
+	local val="$2"
+
+	[ -n "$val" ] && uci set "network.$IFACE.$key=$val"
+}
+
+# добавить option peer если значение существует
+set_peer_opt() {
+	local key="$1"
+	local val="$2"
+
+	[ -n "$val" ] && uci set "network.$PEER.$key=$val"
+}
+
+# добавить list
+add_list() {
+	local section="$1"
+	local key="$2"
+	local vals="$3"
+
+	[ -z "$vals" ] && return
+
+	local OLD_IFS="$IFS"
+	IFS=','
+
+	for v in $vals; do
+		v="$(echo "$v" | xargs)"
+		[ -n "$v" ] && uci add_list "network.$section.$key=$v"
+	done
+
+	IFS="$OLD_IFS"
+}
+
+# =========================
+# Interface
+# =========================
+
+PRIVATE_KEY="$(get_val PrivateKey)"
+ADDRESS="$(get_val Address)"
+DNS="$(get_val DNS)"
+MTU="$(get_val MTU)"
+
+S1="$(get_val S1)"
+S2="$(get_val S2)"
+S3="$(get_val S3)"
+S4="$(get_val S4)"
+
+JC="$(get_val Jc)"
+JMIN="$(get_val Jmin)"
+JMAX="$(get_val Jmax)"
+
+H1="$(get_val H1)"
+H2="$(get_val H2)"
+H3="$(get_val H3)"
+H4="$(get_val H4)"
+
+I1="$(get_val I1)"
+I2="$(get_val I2)"
+I3="$(get_val I3)"
+
+uci set network.$IFACE="interface"
+
+set_opt proto "amneziawg"
+set_opt private_key "$PRIVATE_KEY"
+
+set_opt awg_s1 "$S1"
+set_opt awg_s2 "$S2"
+set_opt awg_s3 "$S3"
+set_opt awg_s4 "$S4"
+
+set_opt awg_jc "$JC"
+set_opt awg_jmin "$JMIN"
+set_opt awg_jmax "$JMAX"
+
+set_opt awg_h1 "$H1"
+set_opt awg_h2 "$H2"
+set_opt awg_h3 "$H3"
+set_opt awg_h4 "$H4"
+
+set_opt awg_i1 "$I1"
+set_opt awg_i2 "$I2"
+set_opt awg_i3 "$I3"
+
+set_opt mtu "$MTU"
+
+uci set network.$IFACE.multipath="off"
+
+add_list "$IFACE" addresses "$ADDRESS"
+add_list "$IFACE" dns "$DNS"
+
+# =========================
+# Peer
+# =========================
+
+PUBLIC_KEY="$(get_val PublicKey)"
+PRESHARED_KEY="$(get_val PresharedKey)"
+ALLOWED_IPS="$(get_val AllowedIPs)"
+ENDPOINT="$(get_val Endpoint)"
+KEEPALIVE="$(get_val PersistentKeepalive)"
+
+ENDPOINT_HOST="${ENDPOINT%:*}"
+ENDPOINT_PORT="${ENDPOINT##*:}"
+
+uci set network.$PEER="amneziawg_$IFACE"
+
+set_peer_opt description "$(basename "$CONF")"
+set_peer_opt public_key "$PUBLIC_KEY"
+set_peer_opt preshared_key "$PRESHARED_KEY"
+
+add_list "$PEER" allowed_ips "$ALLOWED_IPS"
+
+set_peer_opt endpoint_host "$ENDPOINT_HOST"
+set_peer_opt endpoint_port "$ENDPOINT_PORT"
+set_peer_opt persistent_keepalive "$KEEPALIVE"
+
+# сохранить
+uci commit network
+
+# перезапуск сети
+/etc/init.d/network restart
+
+echo "AWG интерфейс успешно создан"
