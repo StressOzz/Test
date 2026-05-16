6c368c0038c72ecffbae3d3043303b93e02122ae
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-05-17 00:05:10 +0500

    Update and rename test10.sh to test11.sh

--- test11.sh
+++ test11.sh
@@ -636,7 +636,7 @@ ENDPOINT="engage.cloudflareclient.com:4500"
 
 
 pkg_is_installed () { local pkg_name="$1"; if [ "$PKG_IS_APK" -eq 1 ]; then apk info -e "$pkg_name" >/dev/null 2>&1; else opkg list-installed | grep -q "^$pkg_name"; fi }
-PODKOP_VER() { LOCALPOD="$(podkop-plus show_version 2>/dev/null | cut -d'-' -f1 | sed 's/^v//')"; if [ -z "$LOCALPOD" ]; then PODKOP_STATUS="${RED}не установлен${NC}"; return; fi; if [ "$PODKOP_LATEST_VER" = "$LOCALPOD" ]
+PODKOP_VER() { LOCALPOD="$(podkop-plus show_version 2>/dev/null)"; if [ -z "$LOCALPOD" ]; then PODKOP_STATUS="${RED}не установлен${NC}"; return; fi; if [ "$PODKOP_LATEST_VER" = "$LOCALPOD" ]
 then PODKOP_STATUS="${GREEN}$LOCALPOD${NC}"; else PODKOP_STATUS="${RED}$LOCALPOD${NC}"; fi; }
 # УСТАНОВКА PODKOP
 PODKOP_INSTALL() { if ! pkg_is_installed podkop-plus; then ACTION="install"; elif [ "$PODKOP_LATEST_VER" != "$LOCALPOD" ]; then ACTION="update"; else ACTION="remove"; fi; if [ "$ACTION" = "install" ] || [ "$ACTION" = "update" ]; then rm -rf "$tmpDIR"; mkdir -p "$tmpDIR"
