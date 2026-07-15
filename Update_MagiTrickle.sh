6613fb5a2c7ce3fd55c5a8532279539366db7aca
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-07-15 21:10:40 +0500

    Update Update_MagiTrickle.sh

--- Update_MagiTrickle.sh
+++ Update_MagiTrickle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-USE_APK=0
-command -v apk >/dev/null 2>&1 && USE_APK=1
+INSTALL="opkg install"; UPDATE="opkg update"; RAZ="ipk"; SUF="r"
+command -v apk >/dev/null 2>&1 && INSTALL="apk add --allow-untrusted" && UPDATE="apk update" && RAZ="apk" && SUF="" 
 
 ARCH_MT=$(grep "^OPENWRT_ARCH=" /etc/os-release | cut -d'"' -f2)
 
@@ -9,47 +9,21 @@ MT_VERSION="$(curl -Ls -o /dev/null -w '%{url_effective}' https://github.com/Mag
 
 [ -z "$MT_VERSION" ] && { echo "Не удалось определить версию"; exit 1; }
 
-URL_APK="https://github.com/MagiTrickle/MagiTrickle/releases/download/${MT_VERSION}/magitrickle_${MT_VERSION}-r1_openwrt_${ARCH_MT}.apk"
-URL_IPK="https://github.com/MagiTrickle/MagiTrickle/releases/download/${MT_VERSION}/magitrickle_${MT_VERSION}-1_openwrt_${ARCH_MT}.ipk"
+FILE_MT="/tmp/magitrickle.$RAZ"
 
-if [ "$USE_APK" -eq 1 ]; then
+URL="https://github.com/MagiTrickle/MagiTrickle/releases/download/${MT_VERSION}/magitrickle_${MT_VERSION}-${SUF}1_openwrt_${ARCH_MT}.$RAZ"
 
-    FILE_MT="/tmp/magitrickle.apk"
+clear
 
-    clear
+echo "Скачивание:"
+echo "$URL"
 
-    echo "Скачивание:"
-    echo "$URL_APK"
+curl -Lf --retry 3 --retry-delay 2 -o "$FILE_MT" "$URL" >/dev/null 2>&1 || { echo "Ошибка скачивания"; exit 1; }
 
-    curl -Lf --retry 3 --retry-delay 2 -o "$FILE_MT" "$URL_APK" >/dev/null 2>&1 || {
-        echo "Ошибка скачивания"
-        exit 1
-    }
+echo "Установка $(basename "$URL")"
 
-    echo "Установка $(basename "$URL_APK")"
+$UPDATE >/dev/null 2>&1 || { echo "Ошибка обновления пакетов"; exit 1; }
 
-    apk add --allow-untrusted "$FILE_MT" >/dev/null 2>&1 || {
-        echo "Ошибка установки"
-        exit 1
-    }
+$INSTALL "$FILE_MT" >/dev/null 2>&1 || { echo "Ошибка установки"; exit 1; }
 
-else
-
-    FILE_MT="/tmp/magitrickle.ipk"
-
-    echo "Скачивание:"
-    echo "$URL_IPK"
-
-    curl -Lf --retry 3 --retry-delay 2 -o "$FILE_MT" "$URL_IPK" >/dev/null 2>&1 || {
-        echo "Ошибка скачивания"
-        exit 1
-    }
-
-    echo "Установка $(basename "$URL_IPK")"
-
-    opkg install "$FILE_MT" >/dev/null 2>&1 || {
-        echo "Ошибка установки"
-        exit 1
-    }
-
-fi
+echo; echo "MagiTrickle установлен (обновлён)"; echo
