1b9c4d986b8d12680ffc22f5aa5265a7eea0e345
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-07-18 04:30:23 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1309,21 +1309,18 @@ EOF
 
 install_magitrickle() {
 
-GREEN="\033[1;32m"; RED="\033[1;31m"; CYAN="\033[1;36m"; YELLOW="\033[1;33m"; MAGENTA="\033[1;35m"; NC="\033[0m"
-INSTALL="opkg install"; UPDATE="opkg update"; RAZ="ipk"; SUF=""
-command -v apk >/dev/null 2>&1 && INSTALL="apk add --allow-untrusted" && UPDATE="apk update" && RAZ="apk" && SUF="r"
-ARCH_MT=$(grep "^OPENWRT_ARCH=" /etc/os-release | cut -d'"' -f2)
-MT_VERSION="0.7.0"
-[ -z "$MT_VERSION" ] && { echo "\n${RED}Не удалось определить версию${NC}\n"; exit 1; }
-FILE_MT="/tmp/magitrickle.$RAZ"
-URL="https://github.com/MagiTrickle/MagiTrickle/releases/download/${MT_VERSION}/magitrickle_${MT_VERSION}-${SUF}1_openwrt_${ARCH_MT}.$RAZ"
-echo -e "${YELLOW}Скачиваем:\n${CYAN}$URL${NC}"
-curl -Lf --retry 3 --retry-delay 2 -o "$FILE_MT" "$URL" >/dev/null 2>&1 || { echo -e "\n${RED}Ошибка скачивания${NC}\n"; exit 1; }
-echo -e "${YELLOW}Устанавливаем:\n${CYAN}$(basename "$URL")${NC}"
-$UPDATE >/dev/null 2>&1 || { echo -e "\n${RED}Ошибка обновления пакетов${NC}\n"; exit 1; }
-$INSTALL "$FILE_MT" >/dev/null 2>&1 || { echo -e "\n${RED}Ошибка установки${NC}\n"; exit 1; }
-echo -e "\n${GREEN}MagiTrickle установлен (обновлён)${NC}\n"
-   
+###################################################################################################
+
+PAUSE() { echo -ne "Нажмите Enter..."; read dummy; }
+INSTALL="opkg install"; RAZ="ipk"; SUF=""
+command -v apk >/dev/null 2>&1 && INSTALL="apk add --allow-untrusted" && RAZ="apk" && SUF="r"
+URL="https://gitlab.com/magitrickle/magitrickle/-/jobs/15410107572/artifacts/raw/.build/magitrickle_0.8.1_pre20260717225233-r1_openwrt_aarch64_cortex-a53.apk"
+FILE_MT="/tmp/$(basename "$URL")"; echo -e "Скачиваем и устанавливаем:\n${CYAN}$URL${NC}"
+curl -Lf --retry 3 --retry-delay 2 -o "$FILE_MT" "$URL" >/dev/null 2>&1 || { echo -e "\n${RED}Ошибка скачивания${NC}\n"; PAUSE; exit 1; }
+$INSTALL "$FILE_MT" >/dev/null 2>&1 || { echo -e "\n${RED}Ошибка установки${NC}\n"; PAUSE; rm -f "$FILE_MT"; exit 1; }
+rm -f "$FILE_MT"
+
+###################################################################################################
 
     echo "Создание страницы MagiTrickle в LuCI"
     mkdir -p /www/luci-static/resources/view/magitrickle
@@ -1474,7 +1471,8 @@ cp -r /tmp/zashboard/* "$DIR1"/ 2>/dev/null || {
 
 rm -rf "$TMP1" "$TMP2"
 
-/etc/init.d/mihomo restart > /dev/null 2>&1
+/etc/init.d/mihomo restart >/dev/null 2>&1
+/etc/init.d/hev-socks5-tunnel restart >/dev/null 2>&1
 
 ###################################################################################################
 
