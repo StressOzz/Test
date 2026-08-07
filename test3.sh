dd4f63bccfe83049e6fa974135f220526cb57e94
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-08-08 03:49:19 +0500

    Update and rename test2.sh to test3.sh

--- test3.sh
+++ test3.sh
@@ -112,7 +112,7 @@ get_ver "https://github.com/yandexru45/netshift/releases/latest" "$TMP_VER_POD" 
 # [ -s "$TMP_MAG_VER" ] && MT_VERSION="$(cat "$TMP_MAG_VER")"
 [ -s "$TMP_VER" ] && ZAPRET_VERSION="$(cat "$TMP_VER")"; [ -s "$TMP_VER_POD" ] && PODKOP_LATEST_VER="$(cat "$TMP_VER_POD")"; [ -s "$TMP_VER_TG_MT" ] && TG_MTProto="$(cat "$TMP_VER_TG_MT")"; [ -s "$TMP_VER_SPL" ] && SPL_VER="$(cat "$TMP_VER_SPL")"
 
-echo 'sh <(wget -q -O - https://raw.githubusercontent.com/StressOzz/Test/main/test2.sh) "$@"' > /usr/bin/zms; chmod +x /usr/bin/zms
+echo 'sh <(wget -q -O - https://raw.githubusercontent.com/StressOzz/Test/main/test3.sh) "$@"' > /usr/bin/zms; chmod +x /usr/bin/zms
 
 # git="githubusercontent.com"; if ! grep -q "raw.$git" /etc/hosts; then echo -e "\n\033[1;36mДля корректной работы скрипта добавляем домены \033[0mGitHub\033[1;36m в \033[0m/etc/hosts\033[0m"
 # printf "#$git\n185.199.109.133 raw.$git release-assets.$git\n185.199.108.133 private-user-images.$git gist.$git avatars.$git\n" >> /etc/hosts; /etc/init.d/dnsmasq restart >/dev/null 2>&1; fi
@@ -428,6 +428,7 @@ sort_results_desc "$AUTO_RESULTS" "$AUTO_RESULTS"
             BLOCK=$(cat "$TEMP_FILE_AUTO")
 
             sed -i "/^[[:space:]]*option NFQWS_OPT '/,\$d" "$CONF"
+echo "$BLOCK"
 
             {
                 echo "  option NFQWS_OPT '"
@@ -440,9 +441,6 @@ sort_results_desc "$AUTO_RESULTS" "$AUTO_RESULTS"
                 esac
                 echo "'"
             } >> "$CONF"
-
-
-echo "$BLOCK"
 
             if ! grep -q "option NFQWS_PORTS_UDP.*19294-19344,50000-50100" "$CONF"; then
                 sed -i "/^[[:space:]]*option NFQWS_PORTS_UDP '/s/'\$/,19294-19344,50000-50100'/" "$CONF"
