97e7e7befe44a224bd7ac68433ddfbfcdb812764
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-08-08 03:32:41 +0500

    Update and rename test1.sh to test2.sh

--- test2.sh
+++ test2.sh
@@ -112,7 +112,7 @@ get_ver "https://github.com/yandexru45/netshift/releases/latest" "$TMP_VER_POD" 
 # [ -s "$TMP_MAG_VER" ] && MT_VERSION="$(cat "$TMP_MAG_VER")"
 [ -s "$TMP_VER" ] && ZAPRET_VERSION="$(cat "$TMP_VER")"; [ -s "$TMP_VER_POD" ] && PODKOP_LATEST_VER="$(cat "$TMP_VER_POD")"; [ -s "$TMP_VER_TG_MT" ] && TG_MTProto="$(cat "$TMP_VER_TG_MT")"; [ -s "$TMP_VER_SPL" ] && SPL_VER="$(cat "$TMP_VER_SPL")"
 
-echo 'sh <(wget -q -O - https://raw.githubusercontent.com/StressOzz/Test/main/test1.sh) "$@"' > /usr/bin/zms; chmod +x /usr/bin/zms
+echo 'sh <(wget -q -O - https://raw.githubusercontent.com/StressOzz/Test/main/test2.sh) "$@"' > /usr/bin/zms; chmod +x /usr/bin/zms
 
 # git="githubusercontent.com"; if ! grep -q "raw.$git" /etc/hosts; then echo -e "\n\033[1;36mДля корректной работы скрипта добавляем домены \033[0mGitHub\033[1;36m в \033[0m/etc/hosts\033[0m"
 # printf "#$git\n185.199.109.133 raw.$git release-assets.$git\n185.199.108.133 private-user-images.$git gist.$git avatars.$git\n" >> /etc/hosts; /etc/init.d/dnsmasq restart >/dev/null 2>&1; fi
@@ -431,7 +431,6 @@ sort_results_desc "$AUTO_RESULTS" "$AUTO_RESULTS"
 
             {
                 echo "  option NFQWS_OPT '"
-                echo "$BLOCK"
                 # Если победила v-стратегия и был оригинальный кастомный Yv —
                 # переносим именно его, а не дефолтный, который добавит ADD_Yv
                 case "$BEST_NAME" in
@@ -442,12 +441,16 @@ sort_results_desc "$AUTO_RESULTS" "$AUTO_RESULTS"
                 echo "'"
             } >> "$CONF"
 
+
+echo "$BLOCK"
+
             if ! grep -q "option NFQWS_PORTS_UDP.*19294-19344,50000-50100" "$CONF"; then
                 sed -i "/^[[:space:]]*option NFQWS_PORTS_UDP '/s/'\$/,19294-19344,50000-50100'/" "$CONF"
             fi
             if ! grep -q "option NFQWS_PORTS_TCP.*2053,2083,2087,2096,8443" "$CONF"; then
                 sed -i "/^[[:space:]]*option NFQWS_PORTS_TCP '/s/'\$/,2053,2083,2087,2096,8443'/" "$CONF"
             fi
+            
             discord_str_add
 
             case "$BEST_NAME" in
