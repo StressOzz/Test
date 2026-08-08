d8b807eba77249f8cedd79ae5bb26237852ea75d
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-08-08 14:04:54 +0500

    Update and rename test18.sh to test19.sh

--- test19.sh
+++ test19.sh
@@ -108,8 +108,8 @@ get_ver "https://github.com/yandexru45/netshift/releases/latest" "$TMP_VER_POD" 
 # [ -s "$TMP_MAG_VER" ] && MT_VERSION="$(cat "$TMP_MAG_VER")"
 [ -s "$TMP_VER" ] && ZAPRET_VERSION="$(cat "$TMP_VER")"; [ -s "$TMP_VER_POD" ] && PODKOP_LATEST_VER="$(cat "$TMP_VER_POD")"; [ -s "$TMP_VER_TG_MT" ] && TG_MTProto="$(cat "$TMP_VER_TG_MT")"; [ -s "$TMP_VER_SPL" ] && SPL_VER="$(cat "$TMP_VER_SPL")"
 
-echo 'sh <(wget -q -O - https://raw.githubusercontent.com/StressOzz/Test/main/test18.sh)' > /usr/bin/zms; chmod +x /usr/bin/zms
-echo 'sh <(wget -q -O - https://raw.githubusercontent.com/StressOzz/Test/main/test18.sh) "$@"' > /usr/bin/zmsA; chmod +x /usr/bin/zmsA
+echo 'sh <(wget -q -O - https://raw.githubusercontent.com/StressOzz/Test/main/test19.sh)' > /usr/bin/zms; chmod +x /usr/bin/zms
+echo 'sh <(wget -q -O - https://raw.githubusercontent.com/StressOzz/Test/main/test19.sh) "$@"' > /usr/bin/zmsA; chmod +x /usr/bin/zmsA
 
 # git="githubusercontent.com"; if ! grep -q "raw.$git" /etc/hosts; then echo -e "\n\033[1;36mДля корректной работы скрипта добавляем домены \033[0mGitHub\033[1;36m в \033[0m/etc/hosts\033[0m"
 # printf "#$git\n185.199.109.133 raw.$git release-assets.$git\n185.199.108.133 private-user-images.$git gist.$git avatars.$git\n" >> /etc/hosts; /etc/init.d/dnsmasq restart >/dev/null 2>&1; fi
@@ -588,10 +588,10 @@ fi
 if [ "$RUNNING" = "1" ]; then
             echo -e "${CYAN}3) ${GREEN}Остановить автоподбор в фоне${NC}"
         else
-            echo -e "${CYAN}3) ${GREEN}Запустить сейчас автоподбор в фоне${NC}"
+            echo -e "${CYAN}3) ${GREEN}Запустить автоподбор в фоне${NC}"
         fi
         [ -s "$AUTO_RESULTS" ] && echo -e "${CYAN}4) ${GREEN}Показать результаты последнего теста${NC}"
-                [ -f "$AUTO_LOG" ] && echo -e "${CYAN}5) ${GREEN}Показать полный лог${NC}"
+                [ -f "$AUTO_LOG" ] && echo -e "${CYAN}5) ${GREEN}Показать полный лог последнего теста${NC}"
         echo -e "${CYAN}6) ${GREEN}Настроить время на роутере${NC}"
         
 if [ -s "$AUTO_RESULTS" ] || [ -f "$AUTO_LOG" ]; then
@@ -607,7 +607,7 @@ fi
             3) if [ "$RUNNING" = "1" ]; then
                    echo -e "\n${MAGENTA}Останавливаем автоподбор${NC}"
                    stop_auto_best
-                   echo -e "${GREEN}Автоподбор остановлен, конфигурация восстановлена!${NC}\n"
+                   echo -e "${GREEN}Автоподбор в фоне остановлен!${NC}\n"
                    PAUSE
                else
                    run_auto_best_background
@@ -1371,7 +1371,7 @@ if auto_best_running; then
     fi
 else
     if [ -n "$HOURAV" ]; then
-        echo -e "${YELLOW}Автоподбор стратегий:${NC}${GREEN}ежедневно в $(printf "%02d" "$HOURAV"):00${NC}"
+        echo -e "${YELLOW}Автоподбор стратегий:${NC}${GREEN}ежедневно в ${NC}$(printf "%02d" "$HOURAV"):00"
     fi
 fi
 
