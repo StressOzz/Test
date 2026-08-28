9920c9bc0e2af312cf55086fb549a468964ba0b6
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-08-28 17:45:07 +0500

    Update and rename test200.sh to test201.sh

--- test201.sh
+++ test201.sh
@@ -2,25 +2,27 @@
 # ==========================================
 # Zapret Manager by StressOzz
 # =========================================
-ZAPRET_MANAGER_VERSION="9.83"; STR_VERSION_AUTOINSTALL="v7"
+ZAPRET_MANAGER_VERSION="9.84"; STR_VERSION_AUTOINSTALL="v7"
 
 GH_RAW_HOST="https://raw.githubusercontent.com"
 GH_MAIN_HOST="https://github.com"
 GH_PROXY="https://gh-proxy.org/"
-GH_CHECK_URL="https://raw.githubusercontent.com/StressOzz/Zapret-Manager/refs/heads/main/files/StrYoutube"
-check_gh_access() { wget -q -T 5 -t 1 -O /dev/null "$GH_CHECK_URL" 2>/dev/null && GH_OK=1 || GH_OK=0; }
-check_gh_access
-if [ "$GH_OK" = "1" ]; then
-    GH_RAW="$GH_RAW_HOST"; GH_MAIN="$GH_MAIN_HOST"
+GH_CHECK_URL="${GH_RAW_HOST}/StressOzz/Zapret-Manager/refs/heads/main/Zapret-Manager.sh"
+
+if wget -q -T 5 -t 2 -O /dev/null "$GH_CHECK_URL" 2>/dev/null; then
+    GH_OK=1
+    GH_RAW="$GH_RAW_HOST"
+    GH_MAIN="$GH_MAIN_HOST"
 else
-    GH_RAW="${GH_PROXY}${GH_RAW_HOST}"; GH_MAIN="${GH_PROXY}${GH_MAIN_HOST}"
+    GH_OK=0
+    GH_RAW="${GH_PROXY}${GH_RAW_HOST}"
+    GH_MAIN="${GH_PROXY}${GH_MAIN_HOST}"
     echo -e "\033[1;33mGitHub напрямую недоступен, используется прокси: ${GH_PROXY}\033[0m"
 fi
 
 
-
-ZAPRET_VERSION="72.20260307"; PODKOP_LATEST_VER="0.9.6"; TG_MTProto="0.9.3"; MT_VERSION="0.8.2"; ZAPRET2_VERSION="1.0.3"
-SPL_VER="26.8.1.3"; TG_GO_VERSION="1.4.1"; TG_RS_VERSION="2.2.4"; BYEDPI_LATEST_VER="0.17.3"
+ZAPRET_VERSION="72.20260307"; PODKOP_LATEST_VER="0.9.6"; TG_MTProto="0.9.3"; MT_VERSION="0.8.2"; ZAPRET2_VERSION="1.0.4"
+SPL_VER="26.8.1.3"; TG_GO_VERSION="1.4.1"; TG_RS_VERSION="2.2.5"; BYEDPI_LATEST_VER="0.17.3"
 
 echo "sh <(wget -q -O - ${GH_RAW}/StressOzz/Zapret-Manager/main/Zapret-Manager.sh)" > /usr/bin/zms; chmod +x /usr/bin/zms
 echo "sh <(wget -q -O - ${GH_RAW}/StressOzz/Zapret-Manager/main/Zapret-Manager.sh) \"\$@\"" > /usr/bin/zmsA; chmod +x /usr/bin/zmsA
