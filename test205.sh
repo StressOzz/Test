c224c82904350d418e00987e57f04d22c86b1e81
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-08-28 17:55:49 +0500

    Update and rename test204.sh to test205.sh

--- test205.sh
+++ test205.sh
@@ -7,9 +7,9 @@ ZAPRET_MANAGER_VERSION="9.84"; STR_VERSION_AUTOINSTALL="v7"
 GH_RAW_HOST="https://raw.githubusercontent.com"
 GH_MAIN_HOST="https://github.com"
 GH_PROXY="https://gh-proxy.org/"
-GH_CHECK_URL="${GH_RAW_HOST}/StressOzz/Zapret-Manager/main/Zapret-Manager.sh"
+GH_CHECK_URL="${GH_RAW_HOST}/StressOzz/Zapret-Manager/refs/heads/main/Zapret-Manager.sh"
 
-if wget -T 5 -t 2 -O /dev/null "$GH_CHECK_URL" 2>/dev/null; then
+if wget -q -T 5 -O /dev/null "$GH_CHECK_URL" 2>/dev/null; then
     GH_OK=1
     GH_RAW="$GH_RAW_HOST"
     GH_MAIN="$GH_MAIN_HOST"
