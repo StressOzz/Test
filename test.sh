d2213a87c151f2326ba08b87a215cef1b73a444a
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-09-13 03:36:19 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -846,6 +846,19 @@ uci commit firewall >/dev/null 2>&1; /etc/init.d/firewall restart >/dev/null 2>&
 # ==========================================
 # Системное меню
 # ==========================================
+LUCI_EDITION="/usr/libexec/rpcd/zapret-manager"
+install_zapret_manager_luci() {
+    if [ -e "$LUCI_EDITION" ]; then
+        echo -e "\n${MAGENTA}Удаляем Zapret Manager LuCI${NC}"
+rm -rf /usr/lib/zapret-manager /usr/libexec/rpcd/zapret-manager /usr/share/luci/menu.d/luci-app-zapret-manager.json /usr/share/rpcd/acl.d/luci-app-zapret-manager.json /www/luci-static/resources/view/zapret-manager /www/luci-static/resources/zapret-manager /tmp/zapret-manager /tmp/luci-indexcache* /tmp/luci-modulecache/* && /etc/init.d/rpcd restart && /etc/init.d/uhttpd restart
+        echo -e "Zapret Manager LuCI ${GREEN}удалён!${NC}\n"
+    else
+        echo -e "\n${MAGENTA}Устанавливаем Zapret Manager LuCI${NC}"
+        sh <(wget -qO - https://raw.githubusercontent.com/StressOzz/Test/refs/heads/main/ZapretManager_LuCI_edition.sh)
+        echo -e "Zapret Manager LuCI ${GREEN}установлен!${NC}\n"
+    fi
+    PAUSE
+}
 is_expert_mode() { [ -f "$EXPERT_MODE_FILE" ]; }; toggle_expert_mode() { [ ! -f /etc/init.d/zapret ] && { echo -e "\nZapret ${RED}не установлен!${NC}\n"; PAUSE; return; }; if is_expert_mode; then rm -f "$EXPERT_MODE_FILE"; echo -e "\nExpert mode ${GREEN}выключен!${NC}\n"; else echo 1 > "$EXPERT_MODE_FILE"; echo -e "\nExpert mode ${GREEN}включён!${NC}\n"; fi; PAUSE; }
 sys_menu() { while true; do web_is_enabled && WEB_TEXT="Удалить доступ к скрипту из браузера" || WEB_TEXT="Активировать доступ к скрипту из браузера"; is_expert_mode && EXPERT_TEXT="${GREEN}Выключить${NC} expert mode" || EXPERT_TEXT="${GREEN}Включить${NC} expert mode"
 quic_is_blocked && QUIC_TEXT="${GREEN}Отключить блокировку${NC} QUIC ${GREEN}${NC}" || QUIC_TEXT="${GREEN}Включить блокировку${NC} QUIC ${GREEN}${NC}"
@@ -861,6 +874,7 @@ ping -6 -c 1 -W 2 google.com >/dev/null 2>&1 && echo -e "${CYAN}9) ${GREEN}Вк�
 FO=$(uci get firewall.@defaults[0].flow_offloading 2>/dev/null); FOHW=$(uci get firewall.@defaults[0].flow_offloading_hw 2>/dev/null); FIX=$(grep -q 'ct original packets ge 30 flow offload @ft;' /usr/share/firewall4/templates/ruleset.uc && echo 1 || echo 0)
 if [ "$FO" = 1 ] || [ "$FOHW" = 1 ] || [ "$FIX" = 1 ]; then if [ "$FIX" = 1 ]; then echo -e "${CYAN}0) ${GREEN}Отключить${NC} FIX ${GREEN}для${NC} Flow Offloading"; else echo -e "${CYAN}0) ${GREEN}Применить${NC} FIX ${GREEN}для${NC} Flow Offloading"; fi; fi
 echo -e "${CYAN}i) ${GREEN}Меню исключений ${NC}IP${GREEN} из ${NC}Zapret\n${CYAN}e) ${GREEN}$EXPERT_TEXT${NC}"; echo -e "${CYAN}y) ${GREEN}Установить пакеты из ${NC}/root/"
+echo -e "${CYAN}w)${NC} $( [ -e "$LUCI_EDITION" ] && echo -e "${CYAN}w) ${GREEN}Удалить ${NC}Zapret Manager LuCI" || echo -e "${CYAN}w) ${GREEN}Установить ${NC}Zapret Manager LuCI" )"
 echo -ne "${CYAN}Enter) ${GREEN}Вернуться в предыдущее меню${NC}\n\n${YELLOW}Выберите пункт:${NC} " && read -r choiceMN; case "$choiceMN" in 1) Sys_Info;; 2) toggle_web;; 3) toggle_quic;; 4) menu_MIR;; e|Е|у|У) toggle_expert_mode;;
 5) [ ! -f /etc/init.d/zapret ] && { echo -e "\nZapret ${RED}не установлен!${NC}\n"; PAUSE; continue; }; stop_zapret "1"; grep -q 'echo "Start Zapret"' /opt/zapret/blockcheck.sh || sed -i $'/^[[:space:]]*read A/a\\\t\techo "Start Zapret"; /etc/init.d/zapret restart >/dev/null 2>&1' /opt/zapret/blockcheck.sh
 echo -e "${GREEN}Ctrl+C - oстановить blockcheck${NC}\n"; chmod +x /opt/zapret/blockcheck.sh; /opt/zapret/blockcheck.sh; start_zapret;; 6) uninstall_zapret_all;; 9) toggle_ipv6;; i|I|Ш|ш) Exclusions_menu;;
@@ -1265,11 +1279,10 @@ menu_TGWS() {
         echo -e "${MAGENTA}Меню sTGWS${NC}\n"
 
 echo -e "${YELLOW}ВНИМАНИЕ! БЕТА-ВЕРСИЯ${NC}"
-echo -e "Данная версия находится на этапе ${YELLOW}бета-тестирования${NC}"
-echo -e "и может содержать ошибки или работать нестабильно!"
+echo -e "Данная версия находится на этапе ${YELLOW}бета-тестирования${NC}!"
 echo -e "В отдельных случаях может потребоваться ${RED}сброс роутера${NC} до заводских настроек!"
-echo -e "${RED}Не устанавливайте эту версию, если не уверены, что сможете устранить возможные проблемы.${NC}"
-echo -e "Рекомендуется использовать другие версии ${YELLOW}TG WS Proxy${NC}."
+echo -e "${RED}Не устанавливайте эту версию, если не уверены, что сможете устранить возможные проблемы!${NC}"
+echo -e "Рекомендуется использовать другие версии ${YELLOW}TG WS Proxy${NC}!"
 
         if [ -n "$(tgws status 2>/dev/null)" ]; then
             echo -e "\n${YELLOW}sTGWS:${NC} ${GREEN}запущен${NC}"
