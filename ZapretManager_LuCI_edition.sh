a32542eeb3a728abddf82e0642dfac321a3e0bfe
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-09-12 22:13:49 +0500

    Update ZapretManager_LuCI_edition.sh

--- ZapretManager_LuCI_edition.sh
+++ ZapretManager_LuCI_edition.sh
@@ -2411,8 +2411,8 @@ return view.extend({
 			E('h2', {}, 'Zapret Manager'),
 			E('span', { 'class': 'zm-header-by' }, 'by StressOzz'),
 			E('div', { 'class': 'zm-header-links' }, [
+				E('a', { 'href': 'http://stresskvn.lol/', 'target': '_blank', 'rel': 'noreferrer' }, 'StressKVN - Обход белых списков! Подключайся!')
 				E('a', { 'href': 'https://t.me/stressozz_manager', 'target': '_blank', 'rel': 'noreferrer' }, 'Сообщество Telegram'),
-				E('a', { 'href': 'http://stresskvn.lol/', 'target': '_blank', 'rel': 'noreferrer' }, 'StressKVN')
 			])
 		]));
 		wrap.appendChild(overviewEl);
