297252150ebb31499420ee7f49cb6fa1427689cd
StressOzz <131419324+StressOzz@users.noreply.github.com>
2026-09-13 03:44:30 +0500

    Update test4.sh

--- test4.sh
+++ test4.sh
@@ -849,13 +849,13 @@ uci commit firewall >/dev/null 2>&1; /etc/init.d/firewall restart >/dev/null 2>&
 LUCI_EDITION="/usr/libexec/rpcd/zapret-manager"
 install_zapret_manager_luci() {
     if [ -e "$LUCI_EDITION" ]; then
-        echo -e "\n${MAGENTA}Удаляем Zapret Manager LuCI${NC}"
+        echo -e "\n${MAGENTA}Удаляем Zapret Manager для LuCI${NC}"
         rm -rf /usr/lib/zapret-manager /usr/libexec/rpcd/zapret-manager /usr/share/luci/menu.d/luci-app-zapret-manager.json /usr/share/rpcd/acl.d/luci-app-zapret-manager.json /www/luci-static/resources/view/zapret-manager /www/luci-static/resources/zapret-manager /tmp/zapret-manager /tmp/luci-indexcache* /tmp/luci-modulecache/* && /etc/init.d/rpcd restart && /etc/init.d/uhttpd restart
-        echo -e "Zapret Manager LuCI ${GREEN}удалён!${NC}\n"
+        echo -e "Zapret Manager ${GREEN}для ${NC}LuCI ${GREEN}удалён!${NC}\n"
     else
         echo -e "\n${MAGENTA}Устанавливаем Zapret Manager LuCI${NC}"
         sh <(wget -qO - https://raw.githubusercontent.com/StressOzz/Zapret-Manager/main/ZapretManager_LuCI.sh)
-        echo -e "Zapret Manager LuCI ${GREEN}установлен!${NC}\n"
+        echo -e "Zapret Manager ${GREEN}для ${NC}LuCI ${GREEN}установлен!${NC}\n"
     fi
     PAUSE
 }
